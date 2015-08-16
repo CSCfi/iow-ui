7cac672e56c2701b2409cdbaebf3492a4bbf9d51
Lauri Hahne <lauri.hahne@gmail.com>
2015-08-16 11:46:23 +0300

    clean-up

--- src/components/restapi/restapi-factory.js
+++ src/components/restapi/restapi-factory.js
@@ -1,128 +1,151 @@
-'use strict';
-
 const jsonld = require('jsonld');
 
 angular.module('myApp.restapi.restapi-factory', [])
+  .config(function restApiConfig($httpProvider) {
+    $httpProvider.interceptors.push(function httpInterceptor($q) {
+      return {
+        response: function responseInterceptor(response) {
+          // Loggers on success
+          console.log(response.headers()['content-type']);
 
-
-.factory('myHttpInterceptor', function ($q) {
-    return {
-        response: function (response) {
-            // Loggers on success
-            console.log(response.headers()['content-type']);
-
-            return response;
+          return response;
         },
-        responseError: function (response) {
-            // Loggers on error
-            return $q.reject(response);
+        responseError: function errorInterceptor(response) {
+          // Loggers on error
+          return $q.reject(response);
         }
-    };
-})
+      };
+    });
+  })
 
- .config(function ($httpProvider) {
-    $httpProvider.interceptors.push('myHttpInterceptor');
-})
+.factory('RestAPI', ['$http', function restApiFactory($http) {
+  const urlBase = 'http://localhost:8084/IOAPI/rest/';
+  const RestAPI = {};
 
-.factory('RestAPI', ['$http', function($http) {
+  RestAPI.getAvailableModels = function() {
 
-    var urlBase = 'http://localhost:8084/IOAPI/rest/';
-    var RestAPI = {};
+    var chainedPromise = $http.get(urlBase + "model-data", {
+      headers: {
+        "Accept": "application/ld+json"
+      },
+      params: {
+        "graph": "default"
+      }
 
-    RestAPI.getAvailableModels = function () {
+    }).then(function(response) {
 
-        var chainedPromise = $http.get(urlBase + "model-data",
-         {
-             headers:{"Accept": "application/ld+json"},
-             params:{"graph":"default"}
+      var context = response.data["@context"];
 
-         }).then(function(response){
+      var frame = {
+        "@type": "sd:Service",
+        "defaultDataset": {
+          "@type": "sd:Dataset",
+          "defaultGraph": {
+            "@type": "sd:Graph"
+          },
+          "availableGraphs": {
+            "namedGraph": {
+              "@embed": true
+            }
+          }
+        }
+      };
 
-          var context = response.data["@context"];
+      frame["@context"] = context;
 
-          var frame = {
-          "@type":"sd:Service",
-          "defaultDataset": {
-                 "@type":"sd:Dataset",
-                 "defaultGraph": {"@type":"sd:Graph"},
-                 "availableGraphs":{
-                     "namedGraph":{"@embed":true}
-                 }
-             }
-          };
+      return jsonld.promises.frame(response.data, frame);
 
-          frame["@context"] = context;
+    });
 
-          return jsonld.promises.frame(response.data, frame);
+    return chainedPromise;
 
-        });
+  };
 
-        return chainedPromise;
+  RestAPI.getModel = function(id) {
 
-    };
+    var chainedPromise = $http.get(urlBase + "model-data", {
+      headers: {
+        "Accept": "application/ld+json"
+      },
+      params: {
+        "graph": id
+      }
 
-    RestAPI.getModel = function(id) {
+    }).then(function(response) {
 
-          var chainedPromise = $http.get(urlBase + "model-data",
-         {
-             headers:{"Accept": "application/ld+json"},
-             params:{"graph":id}
+      var context = response.data["@context"];
 
-         }).then(function(response){
+      context.label = {
+        "@id": "http://www.w3.org/2000/01/rdf-schema#label",
+        "@container": "@language"
+      }
 
-          var context = response.data["@context"];
+      context.comment = {
+        "@id": "http://www.w3.org/2000/01/rdf-schema#comment",
+        "@container": "@language"
+      }
 
-          context.label =  {
-            "@id": "http://www.w3.org/2000/01/rdf-schema#label",
-            "@container": "@language"
-           }
+      var frame = {
+        "@type": "iow:CoreComponentLibrary",
+        "classes": {
+          "property": {
+            "predicate": {
+              "@embed": false
+            },
+            "valueClass": {
+              "@omitDefault": true,
+              "@default": [],
+              "@embed": false
+            }
+          }
+        }
+      };
 
-          context.comment =  {
-            "@id": "http://www.w3.org/2000/01/rdf-schema#comment",
-            "@container": "@language"
-           }
+      frame["@context"] = context;
 
-          var frame = {
-              "@type": "iow:CoreComponentLibrary",
-              "classes": {
-               "property": {
-                 "predicate": {
-                   "@embed": false
-                 },
-                 "valueClass": {
-                   "@omitDefault": true,
-                   "@default": [],
-                   "@embed": false
-                 }
-                }
-               }
-           };
+      return jsonld.promises.frame(response.data, frame);
 
-          frame["@context"] = context;
+    });
 
-          return jsonld.promises.frame(response.data, frame);
+    return chainedPromise;
 
-        });
+  }
 
-        return chainedPromise;
+  RestAPI.getUsers = function() {
+    return $http.get(urlBase + "users", {
+      headers: {
+        "Accept": "application/ld+json"
+      }
+    });
+  };
 
-    }
+  RestAPI.getGroups = function() {
+    return $http.get(urlBase + "groups", {
+      headers: {
+        "Accept": "application/ld+json"
+      }
+    });
+  };
 
-    RestAPI.getUsers = function () {
-        return $http.get(urlBase + "users",{headers:{"Accept": "application/ld+json"}});
-    };
+  RestAPI.newUser = function(name, email) {
+    return $http.put(urlBase + "users", {
+      params: {
+        "fullName": name,
+        "email": email
+      }
+    });
+  };
 
-    RestAPI.getGroups = function () {
-       return $http.get(urlBase + "groups",{headers:{"Accept": "application/ld+json"}});
-    };
+  RestAPI.getUser = function(email) {
+    return $http.get(urlBase + "users", {
+      headers: {
+        "Accept": "application/ld+json"
+      },
+      params: {
+        "email": email
+      }
+    });
+  };
 
-    RestAPI.newUser = function (name,email) {
-        return $http.put(urlBase +"users", {params:{"fullName":name,"email":email}});
-    };
-
-    RestAPI.getUser = function (email) {
-        return $http.get(urlBase + "users",{headers:{"Accept": "application/ld+json"},params:{"email":email}});
-    };
-
-    return RestAPI;
+  return RestAPI;
 }]);
