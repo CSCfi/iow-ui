aa5b9aff5ec7b9c8ba691c8fb142e40d10484391
Lauri Hahne <lauri.hahne@gmail.com>
2015-08-24 08:22:30 +0300

    clean-up main.js

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,11 +1,11 @@
 angular.module('myApp.main', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(function mainConfig($routeProvider) {
   $routeProvider.when('/main', {
     template: require('./main.html'),
     controller: 'MainCtrl'
   });
-}])
+})
+.controller('MainCtrl', function mainController($scope, $location, RestAPI) {
 
-.controller('MainCtrl', ['$scope', '$location', 'RestAPI', function($scope, $location, RestAPI) {
-}]);
+});
