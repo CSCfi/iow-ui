73bb011ff0255ecd092558231a2a8277f11b8404
Lauri Hahne <lauri.hahne@gmail.com>
2015-09-02 09:03:48 +0300

    title as parameter to grouplist

--- src/groups/groupList.js
+++ src/groups/groupList.js
@@ -1,5 +1,8 @@
 module.exports = function /* @ngInject */ groupListDirective() {
   return {
+    scope: {
+      title: '@'
+    },
     template: require('./_groupList.html'),
     controller($scope, RestAPI) {
       $scope.groups = ['foo', 'bar'];
