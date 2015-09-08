fa16b8b48574cc490012b1c9cfe548488b941103
Lauri Hahne <lauri.hahne@gmail.com>
2015-09-08 13:43:15 +0300

    load group info on group page

--- src/controllers/GroupController.js
+++ src/controllers/GroupController.js
@@ -1,7 +1,14 @@
-module.exports = /* @ngInject */ function GroupController($scope, $routeParams, modelService) {
-  $scope.urn = $routeParams.urn;
+const _ = require('lodash');
 
-  modelService.getModelsByGroup($routeParams.urn).then(response => {
+module.exports = /* @ngInject */ function GroupController($scope, $routeParams, $log, groupService, modelService) {
+  const urn = $routeParams.urn;
+
+  groupService.getGroups().then(response => {
+    $scope.group = _.findWhere(response['@graph'], {'@id': urn});
+    $log.debug($scope.group);
+  });
+
+  modelService.getModelsByGroup(urn).then(response => {
     $scope.models = response['@graph'];
   });
 };
