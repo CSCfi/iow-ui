a9bc339cbc4a5fb9cf0b424265a77e7e632adb50
Lauri Hahne <lauri.hahne@gmail.com>
2015-09-09 11:30:45 +0300

    add classview directive

---
+++ src/directives/classView.js
@@ -0,0 +1,9 @@
+module.exports = function /* @ngInject */ classView($log) {
+  return {
+    scope: {},
+    template: require('./templates/classView.html'),
+    controller($scope) {
+
+    }
+  };
+};
--- src/directives/index.js
+++ src/directives/index.js
@@ -3,6 +3,7 @@ const name = 'iow.directives';
 const mod = angular.module(name, []);
 
 mod.directive('groupList', require('./groupList'));
+mod.directive('classView', require('./classView'));
 
 mod.filter('urlencode', function urlencode() {
   return input => {
