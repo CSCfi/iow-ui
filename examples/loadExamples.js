ea4cba19a74ac2403cd86419d66f6952dd7a7a68
amiika <amiika@gmail.com>
2015-09-11 15:22:42 +0300

    Added user examples

--- examples/loadExamples.js
+++ examples/loadExamples.js
@@ -30,6 +30,7 @@ function makeRequest(requestPath, fileName) {
 }
 
 makeRequest('/IOAPI/rest/groups', 'exampleGroups.json');
+makeRequest('/IOAPI/rest/users', 'exampleUsers.json');
 makeRequest('/IOAPI/rest/core?graph=http://urn.fi/urn:nbn:fi:csc-iow-doo#DooLibrary&group=urn:uuid:4925a5ee-3468-45c2-b31d-bc1cf644ef87',
   'exampleModelDOO.json');
 makeRequest('/IOAPI/rest/core?graph=http://urn.fi/urn:nbn:fi:csc-iow-jhs#CoreLibrary&group=urn:uuid:7a389eed-9f66-4d3c-8c6f-199117562d70',
