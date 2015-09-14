da4d2243183a23f470f8ec36e223e87c541b29e4
Lauri Hahne <lauri.hahne@gmail.com>
2015-09-14 15:03:32 +0300

    fix model attribute bindings

--- src/controllers/modelController.js
+++ src/controllers/modelController.js
@@ -3,18 +3,31 @@ module.exports = /* @ngInject */ function modelController($scope, $routeParams, 
     $scope.rawModel = response;
     $scope.model = response['@graph'][0];
 
-    $scope.activateClass = (klass, index) => {
-      $scope.class = klass;
-      $scope.activeClass = index;
+    function clearAll() {
       $scope.attribute = undefined;
       $scope.activeAttribute = undefined;
+      $scope.class = undefined;
+      $scope.activeClass = undefined;
+      $scope.association = undefined;
+      $scope.activeAssociation = undefined;
+    }
+
+    $scope.activateClass = (klass, index) => {
+      clearAll();
+      $scope.class = klass;
+      $scope.activeClass = index;
     };
 
     $scope.activateAttribute = (attribute, index) => {
+      clearAll();
       $scope.attribute = attribute;
       $scope.activeAttribute = index;
-      $scope.class = undefined;
-      $scope.activeClass = undefined;
+    };
+
+    $scope.activateAssociation = (association, index) => {
+      clearAll();
+      $scope.association = association;
+      $scope.activeAssociation = index;
     };
   });
 };
