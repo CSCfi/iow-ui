b677b362d43d0ec5bcc8736f8c68017a2bc5da74
Lauri Hahne <lauri.hahne@gmail.com>
2015-09-15 13:03:50 +0300

    fix indent

--- src/services/frames.js
+++ src/services/frames.js
@@ -25,7 +25,7 @@ function addToContext(context, values) {
 function modelFrame(data) {
   return  {
     '@context': addToContext(data['@context'], {label, comment, title}),
-    requires: {'@embed':false},
+    requires: {'@embed': false},
     classes: {
       property: {
         predicate: {
