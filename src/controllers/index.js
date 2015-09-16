0d0c5a976caa03c473d19c4e76595354bf59298d
Lauri Hahne <lauri.hahne@gmail.com>
2015-09-16 10:52:42 +0300

    casecase

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,7 @@ const name = 'iow.controllers';
 
 const mod = angular.module(name, []);
 
-mod.controller('groupController', require('./GroupController'));
+mod.controller('groupController', require('./groupController'));
 mod.controller('modelController', require('./modelController'));
 
 module.exports = name;
