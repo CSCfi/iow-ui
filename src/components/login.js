82697cc6f1ccc931405d00de3bcddd4e8ee5c45d
Janne Mäki <janne.maki@evident.fi>
2015-10-12 10:38:32 +0300

    Revert to es5 syntax from es6 classes

--- src/components/login.js
+++ src/components/login.js
@@ -1,56 +1,48 @@
 
-export default function directiveConfig() {
+function loginDirective() {
   return {
     scope: {},
     restrict: 'E',
     template: require('./templates/login.html'),
     controllerAs: 'ctrl',
-    controller: LoginController
+    controller: loginController
   };
 };
 
-class LoginController {
-  /*@ngInject*/
-  constructor($modal, userService) {
-    this.$modal = $modal;
-    this.userService = userService;
-  }
-
-  openLogin() {
-    this.$modal.open({
-      template: require('./templates/login-modal.html'),
-      controller: ModalController,
-      controllerAs: 'ctrl'
-    });
-  }
-
-  isLoggedIn() {
-    return this.userService.isLoggedIn();
-  }
-
-  logout() {
-    this.userService.logout();
+function loginController($modal, userService) {
+  "ngInject";
+  return {
+    openLogin: () => {
+      $modal.open({
+        template: require('./templates/login-modal.html'),
+        controller: modalController,
+        controllerAs: 'ctrl'
+      });
+    },
+    isLoggedIn: () => {
+      return userService.isLoggedIn();
+    },
+    logout: () => {
+      userService.logout();
+    }
   }
 }
 
-class ModalController {
-  /*@ngInject*/
-  constructor($modalInstance, $window, userService) {
-    this.$modalInstance = $modalInstance;
-    this.$window = $window;
-    this.userService = userService;
-  }
 
-  fakeLogin() {
-    this.userService.fakeLogin();
-    this.$modalInstance.dismiss();
-  }
-
-  login() {
-    this.$window.location.href = '/api/login';
-  }
-
-  register() {
-    this.$window.location.href = 'http://eduuni.fi/registration.aspx';
+function modalController($modalInstance, $window, userService) {
+  "ngInject";
+  return {
+    fakeLogin: () => {
+      userService.fakeLogin();
+      $modalInstance.dismiss();
+    },
+    login: () => {
+      $window.location.href = '/api/login';
+    },
+    register: () => {
+      $window.location.href = 'http://eduuni.fi/registration.aspx';
+    }
   }
 }
+
+module.exports = loginDirective;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,37 +1,31 @@
 const fakeUser = {};
 
-export default class UserService {
-
-  loggedInUser;
-
-  /*@ngInject*/
-  constructor($http) {
-    this.$http = $http;
-  }
-
-  updateLogin() {
-    this.$http.get('/api/rest/loginstatus').then(statusResponse => {
-      const loggedIn = angular.fromJson(statusResponse.data);
-      if (loggedIn) {
-        this.$http.get('/api/rest/user').then(userResponse => this.loggedInUser = angular.fromJson(userResponse.data));
-      } else {
-        this.loggedInUser = null;
+function userService($http) {
+  "ngInject";
+  return {
+    updateLogin: () => {
+      $http.get('/api/rest/loginstatus').then(statusResponse => {
+        const loggedIn = angular.fromJson(statusResponse.data);
+        if (loggedIn) {
+          $http.get('/api/rest/user').then(userResponse => this.loggedInUser = angular.fromJson(userResponse.data));
+        } else {
+          this.loggedInUser = null;
+        }
+      });
+    },
+    fakeLogin: () => {
+      this.loggedInUser = fakeUser;
+    },
+    isLoggedIn: () => {
+      return this.loggedInUser;
+    },
+    logout: () => {
+      if (this.loggedInUser !== fakeUser) {
+        $http.get('/api/rest/logout');
       }
-    });
-  }
-
-  fakeLogin() {
-    this.loggedInUser = fakeUser;
-  }
-
-  isLoggedIn() {
-    return this.loggedInUser;
-  }
-
-  logout() {
-    if (this.loggedInUser !== fakeUser) {
-      this.$http.get('/api/rest/logout');
+      this.loggedInUser = null;
     }
-    this.loggedInUser = null;
-  }
+  };
 }
+
+module.exports = userService;
