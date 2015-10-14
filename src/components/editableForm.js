df8c22226ad723e388b19ecec34e15da57ab8f22
Janne Mäki <janne.maki@evident.fi>
2015-10-14 14:11:58 +0300

    Cancel editable forms when model language changes

--- src/components/editableForm.js
+++ src/components/editableForm.js
@@ -7,7 +7,7 @@ module.exports = function editableForm($log) {
     transclude: true,
     template: require('./templates/editableForm.html'),
     controllerAs: 'formController',
-    controller($scope, userService) {
+    controller($scope, userService, modelLanguage) {
       'ngInject';
 
       const vm = this;
@@ -16,6 +16,8 @@ module.exports = function editableForm($log) {
       vm.visible = visible;
       vm.show = show;
       vm.cancel = cancel;
+
+      $scope.$watch(modelLanguage.getLanguage, cancel);
 
       function visible() {
         return $scope.form && $scope.form.$visible;
