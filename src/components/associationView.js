107a3c0355befd114de9bd51d44d64ea7be228f6
Lauri Hahne <lauri.hahne@gmail.com>
2015-10-23 10:34:51 +0300

    basic association editing

--- src/components/associationView.js
+++ src/components/associationView.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+
+const contextUtils = require('../services/contextUtils');
+
 module.exports = function associationView($log) {
   'ngInject';
   return {
@@ -6,14 +10,49 @@ module.exports = function associationView($log) {
     },
     restrict: 'E',
     template: require('./templates/associationView.html'),
+    require: '^ngController',
+    link($scope, element, attributes, modelController) {
+      const controller = $scope.ctrl;
+      modelController.registerView(controller);
+      $scope.modelController = modelController;
+      $scope.formController = element.find('editable-form').controller('editableForm');
+    },
     controller($scope, predicateService) {
       'ngInject';
+
+      let context;
+      let originalId;
 
       $scope.$watch('id', id => {
         predicateService.getPredicateById(id, 'associationFrame').then(data => {
           $scope.association = data['@graph'][0];
+          context = data['@context'];
+          originalId = id;
         });
       });
+
+      $scope.updateAssociation = () => {
+        const ld = _.chain($scope.association)
+          .clone()
+          .assign({'@context': context})
+          .value();
+
+        const id = contextUtils.withFullIRI(context, $scope.association['@id']);
+
+        $log.info(JSON.stringify(ld, null, 2));
+
+        return predicateService.updatePredicate(ld, id, originalId).then(() => {
+          originalId = id;
+          $scope.id = id;
+          $scope.modelController.reload();
+        });
+      };
+
+      return {
+        isEditing() {
+          $scope.formController.visible();
+        }
+      };
     }
   };
 };
