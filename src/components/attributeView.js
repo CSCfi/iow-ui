c1dd09de62022d1b76f04b3d6c2a3ced12863d18
Lauri Hahne <lauri.hahne@gmail.com>
2015-10-23 10:32:52 +0300

    improve readability

--- src/components/attributeView.js
+++ src/components/attributeView.js
@@ -33,7 +33,7 @@ module.exports = function attributeView($log) {
       vm.resetModel = resetModel;
       vm.attributeValues = constants.attributeValues;
       // view contract
-      vm.isEditing = isEditing;
+      vm.isEditing = $scope.formController.visible;
 
       $scope.$watch('ctrl.id', id => fetchProperty(id));
       $scope.$watch(modelLanguage.getLanguage, cancelEditing);
@@ -68,10 +68,6 @@ module.exports = function attributeView($log) {
 
       function resetModel() {
         fetchProperty(originalId);
-      }
-
-      function isEditing() {
-        return $scope.formController.visible();
       }
 
       function cancelEditing() {
