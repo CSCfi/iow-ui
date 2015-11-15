955e062e30b1e3d00ef54b624afd427e2f237d0e
Janne Mäki <janne.maki@evident.fi>
2015-11-15 20:00:55 +0200

    Compact component directories

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -5,3 +5,28 @@ module.exports = name;
 mod.directive('windowHeight', require('./windowHeight'));
 mod.directive('iowTypeahead', require('./typeahead'));
 mod.directive('float', require('./float'));
+mod.directive('modalTemplate', require('./modalTemplateDirective'));
+mod.factory('confirmationModal', require('./confirmationModalFactory'));
+
+mod.filter('translateValue', (languageService) => {
+  'ngInject';
+  return (input) => input ? languageService.translate(input) : '';
+});
+
+mod.filter('translateLabel', (translateValueFilter) => {
+  'ngInject';
+  return input => input ? translateValueFilter(input.label) : '';
+});
+
+mod.filter('orderByLabel', (translateLabelFilter, orderByFilter) => {
+  'ngInject';
+  return array => {
+    return orderByFilter(array, translateLabelFilter);
+  };
+});
+
+mod.filter('urlencode', function urlencode() {
+  return input => {
+    return window.encodeURIComponent(input);
+  };
+});
--- src/components/modal/index.js
+++
@@ -1,6 +0,0 @@
-const name = 'iow.components.modal';
-const mod = angular.module(name, []);
-module.exports = name;
-
-mod.directive('modalTemplate', require('./modalTemplateDirective'));
-mod.factory('confirmationModal', require('./confirmationModalFactory'));
