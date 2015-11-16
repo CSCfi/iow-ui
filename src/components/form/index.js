086cb97bf82a284703092e533d11d64b73f405ac
Janne Mäki <janne.maki@evident.fi>
2015-11-16 02:48:23 +0200

    Move model language chooser out of navigation dir

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -6,3 +6,5 @@ mod.directive('idInput', require('./idInput'));
 mod.directive('localizedInput', require('./localizedInput'));
 mod.directive('editable', require('./editable'));
 mod.directive('valueSelect', require('./valueSelect'));
+mod.directive('modelLanguageChooser', require('./modelLanguageChooser'));
+mod.factory('editableController', require('./editableController'));
--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,7 +2,6 @@ const name = 'iow.components.navigation';
 const mod = angular.module(name, []);
 module.exports = name;
 
-mod.directive('globalLanguageChooser', require('./globalLanguageChooser'));
-mod.directive('modelLanguageChooser', require('./modelLanguageChooser'));
 mod.directive('login', require('./login'));
-mod.directive('breadcrumb', require('./../navigation/breadcrumb'));
+mod.directive('globalLanguageChooser', require('./globalLanguageChooser'));
+mod.directive('breadcrumb', require('./breadcrumb'));
