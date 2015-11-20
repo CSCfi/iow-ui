08118a35e09c28a9079476e0a392ef87cde3377b
Janne Mäki <janne.maki@evident.fi>
2015-11-20 11:58:56 +0200

    Bullets to front page

--- src/components/frontPageController.js
+++ src/components/frontPageController.js
@@ -1,4 +1,18 @@
-module.exports = function frontPageController(locationService) {
+module.exports = function frontPageController($scope, $log, locationService, groupService) {
   'ngInject';
+  const vm = this;
+
   locationService.atFrontPage();
+
+  groupService.getGroups().then(groups => {
+    vm.groups = groups;
+  }, error => $log.error(error));
+
+  vm.bullets = [
+    { title: 'What is description?', content: 'What is description content'},
+    { title: 'What is method?', content: 'What is method content'},
+    { title: 'What can I do?', content: 'What can I do content'},
+    { title: 'How?', content: 'How content'}
+  ];
+
 };
--- src/components/group/index.js
+++ src/components/group/index.js
@@ -2,6 +2,5 @@ const name = 'iow.components.group';
 const mod = angular.module(name, []);
 module.exports = name;
 
-mod.directive('groupList', require('./groupList'));
 mod.controller('groupController', require('./groupController'));
 mod.factory('addModelModal', require('./addModelModal'));
