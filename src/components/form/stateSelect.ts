0a8e5d2c1c458738c2ffbb42ffb948a24544c033
Lauri Hahne <lauri.hahne@gmail.com>
2015-11-27 13:04:52 +0200

    admins can select more states

--- src/components/form/stateSelect.ts
+++ src/components/form/stateSelect.ts
@@ -1,19 +1,33 @@
+import {UserService} from '../../services/userService';
+
 export const mod = angular.module('iow.components.form');
 
-const states = ['Unstable', 'Draft', 'Recommendation', 'Deprecated'];
+const states = ['Unstable', 'Draft'];
+const adminStates = ['Recommendation', 'Deprecated'];
 
 mod.directive('stateSelect', () => {
   'ngInject';
   return {
     scope: {
-      state: '='
+      state: '=',
+      model: '='
     },
     restrict: 'E',
     template: '<value-select name="State" values="ctrl.states" value="ctrl.state"></value-select>',
     controllerAs: 'ctrl',
     bindToController: true,
-    controller() {
-      this.states = states;
+    controller(userService: UserService) {
+      'ngInject';
+
+      const group = this.model.graph.isPartOf['@id'];
+      const user = userService.user;
+
+      if (user.isAdminOfGroup(group)) {
+        this.states = states.concat(adminStates);
+      }
+      else {
+        this.states = states;
+      }
     }
   };
 });
