15ab33108def702c28d236ce91720b1a0f78a18a
Janne Mäki <janne.maki@evident.fi>
2015-11-29 19:51:27 +0200

    Disable submits if form not valid or pending async validations

--- src/components/form/editableEntityButtons.ts
+++ src/components/form/editableEntityButtons.ts
@@ -1,3 +1,6 @@
+import IScope = angular.IScope;
+import IAttributes = angular.IAttributes;
+import IFormController = angular.IFormController;
 export const mod = angular.module('iow.components.form');
 
 mod.directive('editableEntityButtons', () => {
@@ -6,7 +9,15 @@ mod.directive('editableEntityButtons', () => {
     scope: {
       ctrl: '=editableController'
     },
+    require: '^form',
     template: require('./editableEntityButtons.html'),
-    transclude: true
+    transclude: true,
+    link($scope: EditableEntityButtonsScope, element: JQuery, attributes: IAttributes, formController: IFormController) {
+      $scope.form = formController;
+    }
   };
 });
+
+interface EditableEntityButtonsScope extends IScope {
+  form: IFormController;
+}
