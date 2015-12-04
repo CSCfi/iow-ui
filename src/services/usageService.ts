038a9da9cc7d9a13ea4098d48d2f5e397d2280fe
Janne Mäki <janne.maki@evident.fi>
2015-12-04 11:25:39 +0200

    Fix return type

--- src/services/usageService.ts
+++ src/services/usageService.ts
@@ -1,13 +1,13 @@
 import IHttpService = angular.IHttpService;
 import IPromise = angular.IPromise;
-import { EntityDeserializer, Uri } from './entities';
+import { EntityDeserializer, Usage, Uri } from './entities';
 
 export class UsageService {
   /* @ngInject */
   constructor(private $http: IHttpService, private entities: EntityDeserializer) {
   }
 
-  getUsages(id: Uri): IPromise<any> {
+  getUsages(id: Uri): IPromise<Usage> {
     return this.$http.get('/api/rest/usage', {params: {id}})
       .then(response => this.entities.deserializeUsage(response.data));
   }
