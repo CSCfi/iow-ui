68135ec054f8ccec9cc1b64f32c6d121160b49c8
Janne Mäki <janne.maki@evident.fi>
2015-12-10 11:59:22 +0200

    Allow error messages to wrap input

--- src/components/form/errorMessages.ts
+++ src/components/form/errorMessages.ts
@@ -1,12 +1,30 @@
 
+import IScope = angular.IScope;
+import IAttributes = angular.IAttributes;
+import INgModelController = angular.INgModelController;
+import ITimeoutService = angular.ITimeoutService;
 export const mod = angular.module('iow.components.form');
 
 mod.directive('errorMessages', () => {
   return {
-    restrict: 'EA',
+    restrict: 'E',
     scope: {
       ngModelController: '='
     },
-    template: require('./errorMessages.html')
+    transclude: true,
+    template: require('./errorMessages.html'),
+    link($scope: ErrorMessagesScope, element: JQuery, attributes: ErrorMessagesAttributes) {
+      if (!attributes.ngModelController) {
+        $scope.ngModelController = element.find('[ng-model]').controller('ngModel');
+      }
+    }
   }
 });
+
+interface ErrorMessagesAttributes extends IAttributes {
+  ngModelController: string;
+}
+
+interface ErrorMessagesScope extends IScope {
+  ngModelController: INgModelController;
+}
