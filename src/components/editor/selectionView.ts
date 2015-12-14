ef37b378491cad4faf1f8dc7852d710040d9c58a
Janne Mäki <janne.maki@evident.fi>
2015-12-14 16:21:34 +0200

    Shrink / enlarge buttons for selection and visualization

--- src/components/editor/selectionView.ts
+++ src/components/editor/selectionView.ts
@@ -2,6 +2,10 @@ import IScope = angular.IScope;
 import IAttributes = angular.IAttributes;
 export const mod = angular.module('iow.components.editor');
 
+export enum Show {
+  Selection = 0, Both = 1, Visualization = 2
+}
+
 mod.directive('selectionView', () => {
   'ngInject';
   return {
@@ -18,18 +22,54 @@ mod.directive('selectionView', () => {
     },
     controllerAs: 'ctrl',
     bindToController: true,
-    controller() {
-      this.visCollapsed = false;
-      this.contentCollapsed = false;
-
-      return {
-        collapseVisualization() {
-          this.visCollapsed = !this.visCollapsed;
-        },
-        collapseContent() {
-          this.contentCollapsed = !this.contentCollapsed;
-        }
-      };
-    }
+    controller: SelectionViewController
   };
 });
+
+class SelectionViewController {
+  show: Show = Show.Both;
+  hideVisualization: boolean;
+
+  /* @ngInject */
+  constructor($scope: IScope) {
+    $scope.$watch(() => this.hideVisualization, hide => this.show = hide ? Show.Selection : Show.Both);
+  }
+
+  enlargeSelection() {
+    this.show--;
+  }
+
+  shrinkSelection() {
+    this.show++;
+  }
+
+  enlargeVisualization() {
+    this.show++;
+  }
+
+  shrinkVisualization() {
+    this.show--;
+  }
+
+  classForVisualization() {
+    switch (this.show) {
+      case Show.Both:
+        return 'col-md-5';
+      case Show.Selection:
+        return 'col-md-1';
+      case Show.Visualization:
+        return 'col-md-11';
+    }
+  }
+
+  classForSelection() {
+    switch (this.show) {
+      case Show.Both:
+        return 'col-md-7';
+      case Show.Selection:
+        return this.hideVisualization ? 'col-md-12' : 'col-md-11';
+      case Show.Visualization:
+        return 'col-md-1';
+    }
+  }
+}
