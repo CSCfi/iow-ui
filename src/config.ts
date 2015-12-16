7bdcc41d7030e9cc49e8c706c78915617cd2ea2b
Janne Mäki <janne.maki@evident.fi>
2015-12-16 15:18:32 +0200

    Explicit parsing of git date

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,15 @@
+import * as moment from 'moment';
+import Moment = moment.Moment;
 
 export interface Config {
   production: boolean
   development: boolean
-  gitDate: Date;
+  gitDate: Moment;
   gitHash: string;
 }
 
 const env = process.env.NODE_ENV;
-const gitDate = new Date(process.env.GIT_DATE);
+const gitDate = moment(process.env.GIT_DATE, 'YYYY-MM-DD HH:mm:ss ZZ');
 const gitHash = process.env.GIT_HASH;
 
 export const config: Config = {
