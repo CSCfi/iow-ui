6d1d92f88cc615a3f3303e145c06a2af5eef8d2d
Janne Mäki <janne.maki@evident.fi>
2016-02-04 16:40:10 +0200

    Unify one big core context to be shared with every frame

--- src/services/frames.ts
+++ src/services/frames.ts
@@ -1,225 +1,132 @@
-import * as _  from 'lodash';
 import { Uri } from './entities';
-
-const label = { '@id': 'http://www.w3.org/2000/01/rdf-schema#label', '@container': '@language' };
-const title = { '@id': 'http://purl.org/dc/terms/title', '@container': '@language' };
-const comment = { '@id': 'http://www.w3.org/2000/01/rdf-schema#comment', '@container': '@language' };
-const example = { '@id': 'http://www.w3.org/2004/02/skos/core#example', '@container': '@language' };
-const prefLabel = { '@id': 'http://www.w3.org/2004/02/skos/core#prefLabel', '@container': '@language' };
-const inScheme = { '@id': 'http://www.w3.org/2004/02/skos/core#inScheme'};
-const datatype = { '@id': 'http://www.w3.org/ns/shacl#datatype', '@type': '@id' };
-const subClassOf = { '@id': 'http://www.w3.org/2000/01/rdf-schema#subClassOf', '@type': '@id' };
-const subPropertyOf = { '@id': 'http://www.w3.org/2000/01/rdf-schema#subPropertyOf', '@type': '@id' };
-const property = { '@id': 'http://www.w3.org/ns/shacl#property', '@type': '@id' };
-const modified = { '@id': 'http://purl.org/dc/terms/modified', '@type': 'http://www.w3.org/2001/XMLSchema#dateTime' };
-const created = { '@id': 'http://purl.org/dc/terms/created', '@type': 'http://www.w3.org/2001/XMLSchema#dateTime' };
-const isDefinedBy = { '@id': 'http://www.w3.org/2000/01/rdf-schema#isDefinedBy', '@type': '@id' };
-const predicate = { '@id': 'http://www.w3.org/ns/shacl#predicate', '@type': '@id' };
-const valueShape = { '@id': 'http://www.w3.org/ns/shacl#valueShape', '@type': '@id' };
-const pattern = { '@id': 'http://www.w3.org/ns/shacl#pattern'};
-const minCount = { '@id': 'http://www.w3.org/ns/shacl#minCount'};
-const maxCount = { '@id': 'http://www.w3.org/ns/shacl#maxCount'};
-const index = { '@id': 'http://www.w3.org/ns/shacl#index'};
-const nodeKind = { '@id': 'http://www.w3.org/ns/shacl#nodeKind', '@type': '@id' };
-const references = { '@id': 'http://purl.org/dc/terms/references', '@type': '@id' };
-const requires = { '@id': 'http://purl.org/dc/terms/requires', '@type': '@id' };
-const imports = { '@id': 'http://www.w3.org/2002/07/owl#imports', '@type': '@id' };
-const versionInfo = { '@id': 'http://www.w3.org/2002/07/owl#versionInfo' };
-const homepage = { '@id': 'http://xmlns.com/foaf/0.1/homepage' };
-const name = { '@id': 'http://xmlns.com/foaf/0.1/name'};
-const hasPart = { '@id': 'http://purl.org/dc/terms/hasPart', '@type': '@id' };
-const preferredXMLNamespaceName = { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespaceName', '@type': 'http://www.w3.org/2001/XMLSchema#string' };
-const preferredXMLNamespacePrefix = { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespacePrefix', '@type': 'http://www.w3.org/2001/XMLSchema#string' };
-const identifier = { '@id': 'http://purl.org/dc/terms/identifier', '@type': 'http://www.w3.org/2001/XMLSchema#string' };
-const range = { '@id': 'http://www.w3.org/2000/01/rdf-schema#range', '@type': '@id' };
-const subject = { '@id': 'http://purl.org/dc/terms/subject', '@type': '@id' };
-const isPartOf = { '@id': 'http://purl.org/dc/terms/isPartOf', '@type': '@id' };
-const isReferencedBy = { '@id': 'http://purl.org/dc/terms/isReferencedBy', '@type': '@id' };
-const isAdminOf = { '@id': 'http://purl.org/dc/terms/isAdminOf', '@type': '@id' };
-const equivalentClass = { '@id' : 'http://www.w3.org/2002/07/owl#equivalentClass', '@type' : '@id' };
-const equivalentProperty = { '@id' : 'http://www.w3.org/2002/07/owl#equivalentProperty', '@type' : '@id' };
-const constraint = { '@id': 'http://www.w3.org/ns/shacl#constraint', '@type': '@id' };
-const or = { '@id': 'http://www.w3.org/ns/shacl#or', '@container': '@set' };
-const and = { '@id': 'http://www.w3.org/ns/shacl#and', '@container': '@set' };
-const foaf = 'http://xmlns.com/foaf/0.1/';
 
 type Frame = {};
 
-function addToContext(context: any, values: any): Frame {
-  return Object.assign({}, context, values);
+const coreContext = {
+  and: { '@id': 'http://www.w3.org/ns/shacl#and', '@container': '@set' },
+  comment: { '@id': 'http://www.w3.org/2000/01/rdf-schema#comment', '@container': '@language' },
+  constraint: { '@id': 'http://www.w3.org/ns/shacl#constraint', '@type': '@id' },
+  created: { '@id': 'http://purl.org/dc/terms/created', '@type': 'http://www.w3.org/2001/XMLSchema#dateTime' },
+  datatype: { '@id': 'http://www.w3.org/ns/shacl#datatype', '@type': '@id' },
+  equivalentClass: { '@id' : 'http://www.w3.org/2002/07/owl#equivalentClass', '@type' : '@id' },
+  equivalentProperty: { '@id' : 'http://www.w3.org/2002/07/owl#equivalentProperty', '@type' : '@id' },
+  example: { '@id': 'http://www.w3.org/2004/02/skos/core#example', '@container': '@language' },
+  foaf: 'http://xmlns.com/foaf/0.1/',
+  hasPart: { '@id': 'http://purl.org/dc/terms/hasPart', '@type': '@id' },
+  homepage: { '@id': 'http://xmlns.com/foaf/0.1/homepage' },
+  identifier: { '@id': 'http://purl.org/dc/terms/identifier', '@type': 'http://www.w3.org/2001/XMLSchema#string' },
+  imports: { '@id': 'http://www.w3.org/2002/07/owl#imports', '@type': '@id' },
+  index: { '@id': 'http://www.w3.org/ns/shacl#index' },
+  inScheme: { '@id': 'http://www.w3.org/2004/02/skos/core#inScheme' },
+  isAdminOf: { '@id': 'http://purl.org/dc/terms/isAdminOf', '@type': '@id' },
+  isDefinedBy: { '@id': 'http://www.w3.org/2000/01/rdf-schema#isDefinedBy', '@type': '@id' },
+  isPartOf: { '@id': 'http://purl.org/dc/terms/isPartOf', '@type': '@id' },
+  isReferencedBy: { '@id': 'http://purl.org/dc/terms/isReferencedBy', '@type': '@id' },
+  label: { '@id': 'http://www.w3.org/2000/01/rdf-schema#label', '@container': '@language' },
+  maxCount: { '@id': 'http://www.w3.org/ns/shacl#maxCount'},
+  minCount: { '@id': 'http://www.w3.org/ns/shacl#minCount'},
+  modified: { '@id': 'http://purl.org/dc/terms/modified', '@type': 'http://www.w3.org/2001/XMLSchema#dateTime' },
+  name: { '@id': 'http://xmlns.com/foaf/0.1/name'},
+  nodeKind: { '@id': 'http://www.w3.org/ns/shacl#nodeKind', '@type': '@id' },
+  or: { '@id': 'http://www.w3.org/ns/shacl#or', '@container': '@set' },
+  pattern: { '@id': 'http://www.w3.org/ns/shacl#pattern' },
+  predicate: { '@id': 'http://www.w3.org/ns/shacl#predicate', '@type': '@id' },
+  preferredXMLNamespaceName: { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespaceName', '@type': 'http://www.w3.org/2001/XMLSchema#string' },
+  preferredXMLNamespacePrefix: { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespacePrefix', '@type': 'http://www.w3.org/2001/XMLSchema#string' },
+  prefLabel: { '@id': 'http://www.w3.org/2004/02/skos/core#prefLabel', '@container': '@language' },
+  property: { '@id': 'http://www.w3.org/ns/shacl#property', '@type': '@id' },
+  range: { '@id': 'http://www.w3.org/2000/01/rdf-schema#range', '@type': '@id' },
+  references: { '@id': 'http://purl.org/dc/terms/references', '@type': '@id' },
+  requires: { '@id': 'http://purl.org/dc/terms/requires', '@type': '@id' },
+  subClassOf: { '@id': 'http://www.w3.org/2000/01/rdf-schema#subClassOf', '@type': '@id' },
+  subject: { '@id': 'http://purl.org/dc/terms/subject', '@type': '@id' },
+  subPropertyOf: { '@id': 'http://www.w3.org/2000/01/rdf-schema#subPropertyOf', '@type': '@id' },
+  title: { '@id': 'http://purl.org/dc/terms/title', '@container': '@language' },
+  valueShape: { '@id': 'http://www.w3.org/ns/shacl#valueShape', '@type': '@id' },
+  versionInfo: { '@id': 'http://www.w3.org/2002/07/owl#versionInfo' }
+};
+
+function frame(data: any, context: {}, frame?: {}) {
+  return Object.assign({ '@context': Object.assign({}, data['@context'], coreContext, context) }, frame);
 }
 
 export function groupFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, comment, homepage})
-  };
+  return frame(data, {});
 }
 
 export function groupListFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, comment, homepage})
-  };
+  return frame(data, {});
 }
 
 export function modelFrame(data: any): Frame {
-  const contextValues = {
-    label,
-    comment,
-    title,
-    example,
-    prefLabel,
-    references,
-    requires,
-    imports,
-    hasPart,
-    preferredXMLNamespaceName,
-    preferredXMLNamespacePrefix,
-    identifier,
-    range,
-    subClassOf,
-    subPropertyOf,
-    property,
-    subject,
-    datatype,
-    predicate,
-    valueShape,
-    nodeKind,
-    versionInfo,
-    homepage,
-    foaf
-  };
-
-  return {
-    '@context': addToContext(data['@context'], contextValues),
-    'isPartOf': {}
-  };
+  return frame(data, {}, { isPartOf: {} });
 }
 
 export function usageFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, isDefinedBy, isReferencedBy}),
-    'isReferencedBy':{}
-  };
+  return frame(data, {}, { isReferencedBy: {} });
 }
 
 export function modelListFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, isPartOf, identifier}),
-    'isPartOf': {}
-  };
+  return frame(data, {}, { isPartOf: {} });
 }
 
 export function propertyFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, range, datatype, valueShape, modified, isDefinedBy, comment, predicate, index}),
-    '@id': data['@id']
-  };
+  return frame(data, {}, { '@id': data['@id'] });
 }
 
 export function predicateListFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, range, modified, isDefinedBy, comment}),
-    'isDefinedBy': {}
-  };
+  return frame(data, {}, { isDefinedBy: {} });
 }
 
 export function predicateFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, prefLabel, range, datatype, valueShape, modified, isDefinedBy, comment, subject, versionInfo, subPropertyOf, equivalentProperty}),
-    'isDefinedBy': {}
-  };
+  return frame(data, {}, { isDefinedBy: {} });
 }
 
 export function classFrame(data: any): Frame {
-  const contextValues = {
-    comment,
-    label,
-    prefLabel,
-    subClassOf,
-    property,
-    modified,
-    isDefinedBy,
-    predicate,
-    valueShape,
-    nodeKind,
-    example,
-    datatype,
-    subject,
-    versionInfo,
-    equivalentClass,
-    index,
-    minCount,
-    maxCount,
-    pattern,
-    constraint,
-    or,
-    and
-  };
-
-  return {
-    '@context': addToContext(data['@context'], contextValues),
-    'isDefinedBy': {}
-  };
+  return frame(data, {}, { isDefinedBy: {} });
 }
 
 export function classListFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, comment, isDefinedBy}),
-    'isDefinedBy': {}
-  };
+  return frame(data, {}, { isDefinedBy: {} });
 }
 
 export function conceptSuggestionFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, comment, inScheme}),
-    'inScheme': {}
-  };
+  return frame(data, {}, { inScheme: {} });
 }
 
 export function fintoConceptFrame(data: any, id: Uri): Frame {
   /* Finto API fix */
-  const value: any = null;
-  const lang: any = null;
-
-  return {
-    '@context': addToContext(data['@context'], {prefLabel, comment, value, lang}),
-    '@id': id
+  const context: any = {
+    value: null,
+    lang: null
   };
+
+  return frame(data, context, { '@id': id });
 }
 
 export function userFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {name, isPartOf, isAdminOf, created, modified}),
-    'name': {}
-  };
+  return frame(data, {}, { name: {} });
 }
 
 export function requireFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, preferredXMLNamespaceName, preferredXMLNamespacePrefix}),
-    'name': {}
-  };
+  return frame(data, {}, { name: {} });
 }
 
 export function searchResultFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, comment}),
-    'name': {}
-  };
+  return frame(data, {}, { name: {} });
 }
 
 export function classVisualizationFrame(data: any): Frame {
-  return {
-    '@context': addToContext(data['@context'], {label, property, datatype, predicate, valueShape, index, minCount, maxCount}),
-    'property': {
-      'predicate': {
+  return frame(data, {}, {
+    property: {
+      predicate: {
         '@embed': false
       },
-      'valueShape': {
+      valueShape: {
         '@omitDefault': true,
         '@default': [],
         '@embed': false
       }
     }
-  };
+  });
 }
