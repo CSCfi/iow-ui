1c8564aa630f8b2904cf36444f2ef8b1dc88f378
Janne Mäki <janne.maki@evident.fi>
2016-02-09 20:07:24 +0200

    Use exclusion predicate instead of set for exclude

--- src/components/editor/addPropertiesFromClassModal.ts
+++ src/components/editor/addPropertiesFromClassModal.ts
@@ -6,40 +6,54 @@ import { Class, Property, Uri } from '../../services/entities';
 import { ClassService } from '../../services/classService';
 import IModalServiceInstance = angular.ui.bootstrap.IModalServiceInstance;
 
+const noExclude = (property: Property) => false;
+
 export class AddPropertiesFromClassModal {
   /* @ngInject */
   constructor(private $uibModal: IModalService) {
   }
 
-  open(classId: Uri, classType: string, excludedPredicates: Set<Uri>): IPromise<Property[]> {
+  open(klass: Uri|Class, classType: string, exclude: (property: Property) => boolean = noExclude): IPromise<Property[]> {
     return this.$uibModal.open({
       template: require('./addPropertiesFromClassModal.html'),
       size: 'adapting',
       controllerAs: 'ctrl',
-      controller: AddPropertiesFromSuperClassModalController,
+      controller: AddPropertiesFromClassModalController,
       resolve: {
-        classId: () => classId,
+        klass: () => klass,
         classType: () => classType,
-        excludedPredicates: () => excludedPredicates
+        exclude: () => exclude
       }
     }).result;
   }
 };
 
-export class AddPropertiesFromSuperClassModalController {
+export class AddPropertiesFromClassModalController {
 
   properties: Property[];
   selectedProperties: Property[];
 
   /* @ngInject */
-  constructor($uibModalInstance: IModalServiceInstance, private classService: ClassService, classId: Uri, public classType: string, excludedPredicates: Set<Uri>) {
-    classService.getClass(classId).then(klass => {
-      this.properties = _.filter(klass.properties, property => !excludedPredicates.has(property.predicateId));
-      this.selectedProperties = _.clone(this.properties);
+  constructor($uibModalInstance: IModalServiceInstance, private classService: ClassService, klass: Uri|Class, public classType: string, private exclude: (property:Property) => boolean) {
 
-      if (this.properties.length === 0) {
-        $uibModalInstance.close(this.properties);
+    const init = (fetchedClass: Class) => {
+      this.properties = fetchedClass.properties;
+      this.selectedProperties = _.reject(this.properties, property => exclude(property));
+
+      if (this.selectedProperties.length === 0) {
+        $uibModalInstance.close(this.selectedProperties);
       }
-    });
+    };
+
+    if (klass instanceof Class) {
+      init(klass);
+    } else {
+      const classId: Uri = <Uri> klass;
+      classService.getClass(classId).then(fetchedClass => init(fetchedClass));
+    }
+  }
+
+  isExcluded(property: Property) {
+    return this.exclude(property);
   }
 }
