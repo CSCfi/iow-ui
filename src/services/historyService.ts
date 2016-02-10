ba76563159d286a3a85f6055ae4f11f376dfe473
Janne Mäki <janne.maki@evident.fi>
2016-02-10 17:53:21 +0200

    Rename version service as history service and use history api endpoint

--- src/services/historyService.ts
+++ src/services/historyService.ts
@@ -2,14 +2,14 @@ import IHttpService = angular.IHttpService;
 import IPromise = angular.IPromise;
 import { Activity, Uri, EntityDeserializer } from './entities';
 
-export class VersionService {
+export class HistoryService {
 
   /* @ngInject */
   constructor(private $http: IHttpService, private entities: EntityDeserializer) {
   }
 
-  getVersion(id: Uri): IPromise<Activity> {
-    return this.$http.get('/api/rest/version', {params: {id}})
+  getHistory(id: Uri): IPromise<Activity> {
+    return this.$http.get('/api/rest/history', {params: {id}})
       .then(response => this.entities.deserializeVersion(response.data));
   }
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -10,7 +10,7 @@ import { SearchService } from './searchService';
 import { UsageService } from './usageService';
 import { UserService } from './userService';
 import { ValidatorService } from './validatorService';
-import { VersionService } from './versionService';
+import { HistoryService } from './historyService';
 
 const mod = angular.module('iow.services', []);
 export = mod.name;
@@ -27,4 +27,4 @@ mod.service('searchService', SearchService);
 mod.service('usageService', UsageService);
 mod.service('userService', UserService);
 mod.service('validatorService', ValidatorService);
-mod.service('versionService', VersionService);
+mod.service('historyService', HistoryService);
