4a7c5417a76b329ab35d9b093f0946b997c668ed
Janne Mäki <janne.maki@evident.fi>
2016-02-23 16:22:27 +0200

    Only serialize root class if exists

--- src/services/entities.ts
+++ src/services/entities.ts
@@ -319,7 +319,7 @@ export class Model extends AbstractModel {
       references: serializeEntityList(this.references),
       requires: serializeEntityList(this.requires),
       identifier: this.version,
-      rootResource: this.rootClass.uri
+      rootResource: this.rootClass && this.rootClass.uri
     }
   }
 }
