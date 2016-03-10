a627284af45a3c6288aa0ff45ba67bb490eadb0a
Janne Mäki <janne.maki@evident.fi>
2016-03-10 13:01:32 +0200

    Show validations formats along with validation messages

--- src/components/form/errorMessages.ts
+++ src/components/form/errorMessages.ts
@@ -3,13 +3,24 @@ import IScope = angular.IScope;
 import IAttributes = angular.IAttributes;
 import INgModelController = angular.INgModelController;
 import ITimeoutService = angular.ITimeoutService;
+import { resolveValidator } from './validators';
+import { dataTypes } from '../common/dataTypes';
 export const mod = angular.module('iow.components.form');
+
+type Error = { key: string, message: string, format?: string };
+const errors: Error[] = [];
+
+for (const dataType of dataTypes) {
+  const format = resolveValidator(dataType).format;
+  errors.push({ key: dataType, message: dataType + ' error', format: format ? `(${format})` : ''});
+}
 
 mod.directive('errorMessages', () => {
   return {
     restrict: 'E',
     template: require('./errorMessages.html'),
     link($scope: ErrorMessagesScope, element: JQuery, attributes: ErrorMessagesAttributes) {
+      $scope.dynamicErrors = errors;
       $scope.$watch(attributes.ngModelController, (ngModelController: INgModelController) => {
         $scope.ngModelController = ngModelController;
       });
@@ -23,4 +34,5 @@ interface ErrorMessagesAttributes extends IAttributes {
 
 interface ErrorMessagesScope extends IScope {
   ngModelController: INgModelController;
+  dynamicErrors: Error[];
 }
