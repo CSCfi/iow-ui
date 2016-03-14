39faf36d52a952d1e92f63169fb49e02e218aaaa
Janne Mäki <janne.maki@evident.fi>
2016-03-14 23:18:36 +0200

    Show loading screen only in production

--- src/init.ts
+++ src/init.ts
@@ -1,17 +1,28 @@
 import './vendor/modernizr';
 import './styles/loading.scss';
+import { config } from './config';
 const pw = require('please-wait');
 
 const logo = require('./assets/logo.png');
 const backgroundColor = '#3F51B5';
 
+function loadingScreen() {
+  if (config.production) {
+    return pw.pleaseWait({
+      logo,
+      backgroundColor,
+      loadingHtml: `<p class='loading-message'>Loading...</p><div class="spinner"></div>`
+    });
+  } else {
+    return {
+      finish() {}
+    }
+  }
+}
+
 if (Modernizr.es5syntax && Modernizr.svg) {
 
-  const waitScreen = pw.pleaseWait({
-    logo,
-    backgroundColor,
-    loadingHtml: `<p class='loading-message'>Loading...</p><div class="spinner"></div>`
-  });
+  let waitScreen = loadingScreen();
 
   require.ensure(['./app'], require => {
     require('./app').done.then(() => waitScreen.finish());
