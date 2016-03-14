48ee8f14c9d10ea18d3daa3e0bfc1666352c25c9
Janne Mäki <janne.maki@evident.fi>
2016-03-14 11:14:47 +0200

    Use ng-options instead of ng-repeat to fix IE9 rendering problems
    Closes #334

--- src/components/form/valueSelect.ts
+++ src/components/form/valueSelect.ts
@@ -1,4 +1,6 @@
 import gettextCatalog = angular.gettext.gettextCatalog;
+import IScope = angular.IScope;
+import * as _ from 'lodash';
 
 export const mod = angular.module('iow.components.form');
 
@@ -11,14 +13,19 @@ mod.directive('valueSelect', () => {
       id: '@'
     },
     restrict: 'E',
-    template: `<select id="{{ctrl.id}}" class="form-control" required ng-model="ctrl.value">
-                 <option ng-repeat="value in ctrl.values" value="{{value}}">{{ctrl.displayName(value)}}</option>
+    template: `<select id="{{ctrl.id}}" class="form-control" required ng-model="ctrl.value" 
+                       ng-options="value.value as value.localization for value in ctrl.localizedValues">
                </select>`,
     controllerAs: 'ctrl',
     bindToController: true,
-    controller(gettextCatalog: gettextCatalog) {
+    controller($scope: IScope, gettextCatalog: gettextCatalog) {
       'ngInject';
-      this.displayName = (text: string) => text && `${gettextCatalog.getString(text)} (${text})`;
+
+      function localize(text: string) {
+        return {localization: text && `${gettextCatalog.getString(text)} (${text})`, value: text};
+      }
+
+      $scope.$watch(() => this.values, values => this.localizedValues = _.map(values, localize));
     }
   };
 });
