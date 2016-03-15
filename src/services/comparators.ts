d501aae594312dfe9ee99bb8ffe6f672c625b8dd
Janne Mäki <janne.maki@evident.fi>
2016-03-15 14:10:17 +0200

    Refactoring

--- src/services/comparators.ts
+++ src/services/comparators.ts
@@ -1,3 +1,4 @@
+import { isDefined } from './utils';
 
 export interface Comparator<T> {
   (lhs: T, rhs: T): number
@@ -7,58 +8,56 @@ export interface ChainableComparator<T> extends Comparator<T> {
   andThen(other: Comparator<T>): ChainableComparator<T>;
 }
 
-function afterComparingExistence<T>(l: T, r: T, compare: (l: T, r: T) => boolean): number {
-  if (l === r) {
-    return 0;
-  } else if (!!l && !r) {
+function compare<T>(l: T, r: T, compareValues: (l: T, r: T) => number): number {
+  if (isDefined(l) && !isDefined(r)) {
     return 1;
-  } else if (!l && !!r) {
+  } else if (!isDefined(l) && isDefined(r)) {
     return -1;
   } else {
-    return compare(l, r) ? 1 : -1;
+    return compareValues(l, r);
   }
 }
 
 export function compareStrings(l: string, r: string): number {
-  return afterComparingExistence(l, r, (lhs, rhs) => lhs > rhs);
+  return compare(l, r, (lhs, rhs) => lhs === rhs ? 0 : lhs > rhs ? 1 : -1);
 }
 
 export function compareNumbers(l: number, r: number): number {
-  return afterComparingExistence(l, r, (lhs, rhs) => lhs > rhs);
+  return compare(l, r, (lhs, rhs) => lhs === rhs ? 0 : lhs > rhs ? 1 : -1);
 }
 
 export function compareBooleans(l: boolean, r: boolean): number {
-  return afterComparingExistence(l, r, (lhs, rhs) => lhs === true);
+  return compare(l, r, (lhs, rhs) => lhs === rhs ? 0 : lhs === true ? 1 : -1);
 }
 
 export function reversed<T>(comparator: Comparator<T>): ChainableComparator<T> {
-  return addChaining((lhs: T, rhs: T) => comparator(rhs, lhs));
+  return makeChainable((lhs: T, rhs: T) => comparator(rhs, lhs));
 }
 
 export function comparingString<T>(extractor: (item: T) => string): ChainableComparator<T> {
-  return addChaining((lhs: T, rhs: T) => compareStrings(extractor(lhs), extractor(rhs)));
+  return makeChainable((lhs: T, rhs: T) => compareStrings(extractor(lhs), extractor(rhs)));
 }
 
 export function comparingNumber<T>(extractor: (item: T) => number): ChainableComparator<T> {
-  return addChaining((lhs: T, rhs: T) => compareNumbers(extractor(lhs), extractor(rhs)));
+  return makeChainable((lhs: T, rhs: T) => compareNumbers(extractor(lhs), extractor(rhs)));
 }
 
 export function comparingBoolean<T>(extractor: (item: T) => boolean): ChainableComparator<T> {
-  return addChaining((lhs: T, rhs: T) => compareBooleans(extractor(lhs), extractor(rhs)));
+  return makeChainable((lhs: T, rhs: T) => compareBooleans(extractor(lhs), extractor(rhs)));
 }
 
-function chain<T>(current: Comparator<T>, next: Comparator<T>): ChainableComparator<T> {
-  return addChaining((lhs: T, rhs: T) => {
+export function makeChainable<T>(comparator: Comparator<T>): ChainableComparator<T> {
+  (<any> comparator).andThen = (next: Comparator<T>) => makeChainable(chain(comparator, next));
+  return <ChainableComparator<T>> comparator;
+}
+
+export function chain<T>(current: Comparator<T>, next: Comparator<T>): Comparator<T> {
+  return (lhs: T, rhs: T) => {
     const currentComparison = current(lhs, rhs);
     if (currentComparison !== 0) {
       return currentComparison;
     } else {
       return next(lhs, rhs);
     }
-  });
-}
-
-function addChaining<T>(comparator: Comparator<T>): ChainableComparator<T> {
-  (<any> comparator).andThen = (next: Comparator<T>) => chain(comparator, next);
-  return <ChainableComparator<T>> comparator;
+  };
 }
