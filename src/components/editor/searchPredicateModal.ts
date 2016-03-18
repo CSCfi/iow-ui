6cc97bbbf31213e34ed2911bc6eb8d2f3880f0b2
Janne Mäki <janne.maki@evident.fi>
2016-03-18 14:23:46 +0200

    Use edit in progress infra from search results

--- src/components/editor/searchPredicateModal.ts
+++ src/components/editor/searchPredicateModal.ts
@@ -9,7 +9,6 @@ import { SearchConceptModal, EntityCreation } from './searchConceptModal';
 import { LanguageService } from '../../services/languageService';
 import { EditableForm } from '../form/editableEntityController';
 import { comparingString, comparingBoolean, reversed } from '../../services/comparators';
-import { ConfirmationModal } from '../common/confirmationModal';
 import { AddNew } from '../common/searchResults';
 import gettextCatalog = angular.gettext.gettextCatalog;
 
@@ -58,6 +57,7 @@ export class SearchPredicateController {
 
   private predicates: PredicateListItem[];
 
+  editInProgress = () => this.$scope.form.editing;
   close = this.$uibModalInstance.dismiss;
   searchResults: (PredicateListItem|AddNewPredicate)[] = [];
   selectedPredicate: Predicate;
@@ -79,7 +79,6 @@ export class SearchPredicateController {
               private predicateService: PredicateService,
               private languageService: LanguageService,
               private searchConceptModal: SearchConceptModal,
-              private confirmationModal: ConfirmationModal,
               private gettextCatalog: gettextCatalog) {
 
     predicateService.getAllPredicates().then((allPredicates: PredicateListItem[]) => {
@@ -133,24 +132,13 @@ export class SearchPredicateController {
   }
 
   selectItem(predicate: PredicateListItem|AddNewPredicate) {
-
-    const that = this;
-
-    function doSelection() {
-      if (predicate instanceof AddNewPredicate) {
-        that.createNew(predicate.type);
-      } else if (predicate instanceof PredicateListItem) {
-        that.$scope.form.editing = false;
-        that.submitError = null;
-        that.cannotConfirm = that.exclude(predicate);
-        that.predicateService.getPredicate(predicate.id, that.model).then(result => that.selectedPredicate = result);
-      }
-    }
-
-    if (this.$scope.form.editing) {
-      this.confirmationModal.openEditInProgress().then(doSelection);
-    } else {
-      doSelection();
+    if (predicate instanceof AddNewPredicate) {
+      this.createNew(predicate.type);
+    } else if (predicate instanceof PredicateListItem) {
+      this.$scope.form.editing = false;
+      this.submitError = null;
+      this.cannotConfirm = this.exclude(predicate);
+      this.predicateService.getPredicate(predicate.id, this.model).then(result => this.selectedPredicate = result);
     }
   }
 
