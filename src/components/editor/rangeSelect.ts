f5650f0518dc73f54065c2baacc6230290280949
Janne Mäki <janne.maki@evident.fi>
2016-04-18 13:36:50 +0300

    Better static typing for range select

--- src/components/editor/rangeSelect.ts
+++ src/components/editor/rangeSelect.ts
@@ -1,5 +1,4 @@
-import { dataTypes } from '../common/dataTypes';
-
+import { dataTypes, DataType } from '../common/dataTypes';
 import { module as mod }  from './module';
 
 mod.directive('rangeSelect', () => {
@@ -12,8 +11,11 @@ mod.directive('rangeSelect', () => {
     template: '<localized-select id="{{ctrl.id}}" values="ctrl.ranges" value="ctrl.range"></localized-select>',
     controllerAs: 'ctrl',
     bindToController: true,
-    controller() {
-      this.ranges = dataTypes;
-    }
+    controller: RangeSelectController
   };
 });
+
+class RangeSelectController {
+  range: DataType;
+  ranges: DataType[] = dataTypes;
+}
