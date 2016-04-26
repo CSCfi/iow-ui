2446bf76ba7455052799a259221c6d4e92bcfef7
Janne Mäki <janne.maki@evident.fi>
2016-04-26 11:55:06 +0300

    Move some not so core definitions out of core context

--- src/services/frames.ts
+++ src/services/frames.ts
@@ -3,43 +3,36 @@ import { Url } from './uri';
 export type Frame = {};
 export type FrameFn = (data: any) => Frame;
 
+const inScheme = { '@id': 'http://www.w3.org/2004/02/skos/core#inScheme', '@type': '@id' };
+const subject = { '@id': 'http://purl.org/dc/terms/subject', '@type': '@id' };
+
 const coreContext = {
-  and: { '@id': 'http://www.w3.org/ns/shacl#and', '@container': '@list' },
   comment: { '@id': 'http://www.w3.org/2000/01/rdf-schema#comment', '@container': '@language' },
-  constraint: { '@id': 'http://www.w3.org/ns/shacl#constraint', '@type': '@id' },
   created: { '@id': 'http://purl.org/dc/terms/created', '@type': 'http://www.w3.org/2001/XMLSchema#dateTime' },
-  datatype: { '@id': 'http://www.w3.org/ns/shacl#datatype', '@type': '@id' },
   definition: {'@id': 'http://www.w3.org/2004/02/skos/core#definition', '@container': '@language' },
-  equivalentClass: { '@id' : 'http://www.w3.org/2002/07/owl#equivalentClass', '@type' : '@id' },
-  equivalentProperty: { '@id' : 'http://www.w3.org/2002/07/owl#equivalentProperty', '@type' : '@id' },
   foaf: 'http://xmlns.com/foaf/0.1/',
   hasPart: { '@id': 'http://purl.org/dc/terms/hasPart', '@type': '@id' },
   homepage: { '@id': 'http://xmlns.com/foaf/0.1/homepage' },
   identifier: { '@id': 'http://purl.org/dc/terms/identifier' },
   imports: { '@id': 'http://www.w3.org/2002/07/owl#imports', '@type': '@id' },
-  inScheme: { '@id': 'http://www.w3.org/2004/02/skos/core#inScheme', '@type': '@id' },
-  isAdminOf: { '@id': 'http://purl.org/dc/terms/isAdminOf', '@type': '@id' },
   isDefinedBy: { '@id': 'http://www.w3.org/2000/01/rdf-schema#isDefinedBy', '@type': '@id' },
   isPartOf: { '@id': 'http://purl.org/dc/terms/isPartOf', '@type': '@id' },
-  isReferencedBy: { '@id': 'http://purl.org/dc/terms/isReferencedBy', '@type': '@id' },
   label: { '@id': 'http://www.w3.org/2000/01/rdf-schema#label', '@container': '@language' },
   modified: { '@id': 'http://purl.org/dc/terms/modified', '@type': 'http://www.w3.org/2001/XMLSchema#dateTime' },
   nodeKind: { '@id': 'http://www.w3.org/ns/shacl#nodeKind', '@type': '@id' },
-  or: { '@id': 'http://www.w3.org/ns/shacl#or', '@container': '@list' },
-  predicate: { '@id': 'http://www.w3.org/ns/shacl#predicate', '@type': '@id' },
-  preferredXMLNamespaceName: { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespaceName' },
-  preferredXMLNamespacePrefix: { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespacePrefix' },
   prefLabel: { '@id': 'http://www.w3.org/2004/02/skos/core#prefLabel', '@container': '@language' },
   prov: "http://www.w3.org/ns/prov#",
-  range: { '@id': 'http://www.w3.org/2000/01/rdf-schema#range', '@type': '@id' },
-  subject: { '@id': 'http://purl.org/dc/terms/subject', '@type': '@id' },
-  subPropertyOf: { '@id': 'http://www.w3.org/2000/01/rdf-schema#subPropertyOf', '@type': '@id' },
   title: { '@id': 'http://purl.org/dc/terms/title', '@container': '@language' },
-  valueShape: { '@id': 'http://www.w3.org/ns/shacl#valueShape', '@type': '@id' },
   versionInfo: { '@id': 'http://www.w3.org/2002/07/owl#versionInfo' }
 };
 
-const predicateContext = Object.assign({}, coreContext, {});
+const predicateContext = Object.assign({}, coreContext, {
+  range: { '@id': 'http://www.w3.org/2000/01/rdf-schema#range', '@type': '@id' },
+  subPropertyOf: { '@id': 'http://www.w3.org/2000/01/rdf-schema#subPropertyOf', '@type': '@id' },
+  equivalentProperty: { '@id' : 'http://www.w3.org/2002/07/owl#equivalentProperty', '@type' : '@id' },
+  datatype: { '@id': 'http://www.w3.org/ns/shacl#datatype', '@type': '@id' },
+  subject
+});
 
 const propertyContext = Object.assign({}, coreContext, predicateContext, {
   index: { '@id': 'http://www.w3.org/ns/shacl#index' },
@@ -52,14 +45,21 @@ const propertyContext = Object.assign({}, coreContext, predicateContext, {
   in: { '@id': 'http://www.w3.org/ns/shacl#in'},
   hasValue: { '@id': 'http://www.w3.org/ns/shacl#hasValue'},
   pattern: { '@id': 'http://www.w3.org/ns/shacl#pattern' },
-  type: { '@id': 'http://purl.org/dc/terms/type', '@type': '@id' }
+  type: { '@id': 'http://purl.org/dc/terms/type', '@type': '@id' },
+  valueShape: { '@id': 'http://www.w3.org/ns/shacl#valueShape', '@type': '@id' },
+  predicate: { '@id': 'http://www.w3.org/ns/shacl#predicate', '@type': '@id' }
 });
 
 const classContext = Object.assign({}, coreContext, propertyContext, {
   abstract: { '@id': 'http://www.w3.org/ns/shacl#abstract'},
   property: { '@id': 'http://www.w3.org/ns/shacl#property', '@type': '@id' },
   scopeClass : { '@id' : 'http://www.w3.org/ns/shacl#scopeClass', '@type' : '@id' },
-  subClassOf: { '@id': 'http://www.w3.org/2000/01/rdf-schema#subClassOf', '@type': '@id' }
+  subClassOf: { '@id': 'http://www.w3.org/2000/01/rdf-schema#subClassOf', '@type': '@id' },
+  equivalentClass: { '@id' : 'http://www.w3.org/2002/07/owl#equivalentClass', '@type' : '@id' },
+  constraint: { '@id': 'http://www.w3.org/ns/shacl#constraint', '@type': '@id' },
+  or: { '@id': 'http://www.w3.org/ns/shacl#or', '@container': '@list' },
+  and: { '@id': 'http://www.w3.org/ns/shacl#and', '@container': '@list' },
+  subject
 });
 
 const versionContext = Object.assign({}, coreContext, {
@@ -73,7 +73,12 @@ const versionContext = Object.assign({}, coreContext, {
 
 const groupContext = Object.assign({}, coreContext, {});
 
-const modelContext = Object.assign({}, coreContext, {
+const requireContext = Object.assign({}, coreContext, {
+  preferredXMLNamespaceName: { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespaceName' },
+  preferredXMLNamespacePrefix: { '@id': 'http://purl.org/ws-mmi-dc/terms/preferredXMLNamespacePrefix' }
+});
+
+const modelContext = Object.assign({}, coreContext, requireContext, {
   rootResource : { '@id' : 'http://rdfs.org/ns/void#rootResource',  '@type' : '@id' },
   references: { '@id': 'http://purl.org/dc/terms/references', '@type': '@id' },
   requires: { '@id': 'http://purl.org/dc/terms/requires', '@type': '@id' },
@@ -82,14 +87,19 @@ const modelContext = Object.assign({}, coreContext, {
   language: { '@id': 'http://purl.org/dc/terms/language', '@container': '@list' }
 });
 
-const usageContext = Object.assign({}, coreContext, {});
-const conceptContext = Object.assign({}, coreContext, {});
-
-const userContext = Object.assign({}, coreContext, {
-  name: { '@id': 'http://xmlns.com/foaf/0.1/name'}
+const usageContext = Object.assign({}, coreContext, {
+  isReferencedBy: { '@id': 'http://purl.org/dc/terms/isReferencedBy', '@type': '@id' }
 });
 
-const requireContext = Object.assign({}, coreContext, {});
+const conceptContext = Object.assign({}, coreContext, {
+  inScheme
+});
+
+const userContext = Object.assign({}, coreContext, {
+  name: { '@id': 'http://xmlns.com/foaf/0.1/name'},
+  isAdminOf: { '@id': 'http://purl.org/dc/terms/isAdminOf', '@type': '@id' }
+});
+
 const searchResultContext = Object.assign({}, coreContext, {});
 
 function frame(data: any, context: {}, frame?: {}) {
@@ -154,7 +164,8 @@ export function fintoConceptFrame(data: any, id: Url): Frame {
     uri: null,
     type: null,
     graph: null,
-    comment: { '@id': 'http://www.w3.org/2000/01/rdf-schema#comment', '@container': '@language' }
+    comment: { '@id': 'http://www.w3.org/2000/01/rdf-schema#comment', '@container': '@language' },
+    inScheme
   });
 
   return frame(data, context, { '@id': id });
