4f8d682db8bb1669ba5dd3883c8dfafc58b4da75
Janne Mäki <janne.maki@evident.fi>
2016-05-04 14:35:43 +0300

    Allow passing duplicate checking function instead of values to check against

--- src/components/form/restrictDuplicates.ts
+++ src/components/form/restrictDuplicates.ts
@@ -3,10 +3,9 @@ import INgModelController = angular.INgModelController;
 import IScope = angular.IScope;
 import * as _ from 'lodash';
 import { Uri } from '../../services/uri';
-
 import { module as mod }  from './module';
 
-interface RestrictDupicatesAttributes extends IAttributes {
+interface RestrictDuplicatesAttributes extends IAttributes {
   restrictDuplicates: string;
 }
 
@@ -14,27 +13,39 @@ mod.directive('restrictDuplicates', () => {
   return {
     restrict: 'A',
     require: ['restrictDuplicates', 'ngModel'],
-    link($scope: IScope, element: JQuery, attributes: RestrictDupicatesAttributes, [thisController, ngModel]: [RestrictDuplicatesController, INgModelController]) {
+    link($scope: IScope, element: JQuery, attributes: RestrictDuplicatesAttributes, [thisController, ngModel]: [RestrictDuplicatesController, INgModelController]) {
 
-      $scope.$watchCollection(attributes.restrictDuplicates, (values: any[]) => {
-        thisController.valuesToCheckAgainst = values;
-      });
+      const restrictDuplicatesValue = $scope.$eval(attributes.restrictDuplicates);
+
+      if (typeof $scope.$eval(attributes.restrictDuplicates) === 'function') {
+        thisController.duplicateCheckingFunction = restrictDuplicatesValue;
+      } else {
+        $scope.$watchCollection(attributes.restrictDuplicates, (values: any[]) => {
+          thisController.valuesToCheckAgainst = values;
+        });
+      }
 
       ngModel.$validators['duplicate'] = value => {
-        const valuesToCheckAgainst = thisController.valuesToCheckAgainst;
 
-        if (!valuesToCheckAgainst) {
-          return true;
-        }
-
-        if ('localizedInput' in attributes) {
-          return _.intersection(Object.values(value), _.flatten(_.map(valuesToCheckAgainst, v => Object.values(v)))).length === 0;
+        if (!!thisController.duplicateCheckingFunction) {
+          return thisController.duplicateCheckingFunction(value);
         } else {
-          if (value instanceof Uri) {
-            return !_.find(valuesToCheckAgainst, valueToCheckAgainst => valueToCheckAgainst.equals(value));
-          } else {
-            return valuesToCheckAgainst.indexOf(value) === -1;
+          const valuesToCheckAgainst = thisController.valuesToCheckAgainst;
+
+          if (!valuesToCheckAgainst) {
+            return true;
           }
+
+          if ('localizedInput' in attributes) {
+            return _.intersection(Object.values(value), _.flatten(_.map(valuesToCheckAgainst, v => Object.values(v)))).length === 0;
+          } else {
+            if (value instanceof Uri) {
+              return !_.find(valuesToCheckAgainst, valueToCheckAgainst => valueToCheckAgainst.equals(value));
+            } else {
+              return valuesToCheckAgainst.indexOf(value) === -1;
+            }
+          }
+
         }
       };
     },
@@ -44,5 +55,6 @@ mod.directive('restrictDuplicates', () => {
 
 
 class RestrictDuplicatesController {
+  duplicateCheckingFunction: (item: any) => boolean;
   valuesToCheckAgainst: any[] = [];
 }
