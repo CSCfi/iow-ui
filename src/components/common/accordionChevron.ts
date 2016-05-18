565b21dee7c6d54db23d4e41e8e529e24a08848a
Jarmo Korhonen <jarmo.korhonen@gofore.com>
2016-05-18 12:18:34 +0300

    Change some glyphicons into fa-icons

--- src/components/common/accordionChevron.ts
+++ src/components/common/accordionChevron.ts
@@ -8,7 +8,7 @@ mod.directive('accordionChevron', () => {
       noPull: '=?'
     },
     transclude: true,
-    template: `<ng-transclude></ng-transclude><span ng-class="['glyphicon', {'pull-right': !noPull,'glyphicon-chevron-down': isOpen, 'glyphicon-chevron-right': !isOpen}]"></span>`,
+    template: `<ng-transclude></ng-transclude><span ng-class="['fa', {'pull-right': !noPull,'fa-angle-down': isOpen, 'fa-angle-right': !isOpen}]"></span>`,
     require: '^uibAccordionGroup'
   };
 });
