313e5faaba07547d8e2ac9a3e6e636915d67f22f
Janne Mäki <janne.maki@evident.fi>
2016-06-16 15:23:30 +0300

    End to end testing infra in place

---
+++ protractor.conf.ts
@@ -0,0 +1,41 @@
+/// <reference path="./src/node.d.ts" />
+
+import * as path from 'path';
+
+function root(...globs: string[]) {
+  return path.join.apply(path, [__dirname].concat(globs));
+}
+
+exports.config = {
+  baseUrl: 'http://localhost:9001/',
+
+  specs: [
+    root('src/**/**.e2e.ts'),
+    root('src/**/*.e2e.ts')
+  ],
+  exclude: [],
+
+  framework: 'jasmine2',
+
+  allScriptsTimeout: 110000,
+
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: false,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000
+  },
+  directConnect: true,
+
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['show-fps-counter=true']
+    }
+  },
+
+  onPrepare: function() {
+    browser.ignoreSynchronization = true;
+  }
+};
---
+++ src/app.e2e.ts
@@ -0,0 +1,12 @@
+describe('App', () => {
+
+  beforeEach(() => {
+    browser.get('/');
+  });
+
+  it('should have a title', () => {
+    let subject = browser.getTitle();
+    let result  = 'IOW';
+    expect(subject).toEqual(result);
+  });
+});
