b7014e4c6b3d493c8fa4da577378f191bfe47580
Janne Mäki <janne.maki@evident.fi>
2016-08-09 12:32:21 +0300

    Check footer existence

--- src/app.e2e.ts
+++ src/app.e2e.ts
@@ -7,4 +7,8 @@ describe('App', () => {
   it('should have a title', () => {
     expect(browser.getTitle()).toBe('IOW');
   });
+
+  it('should have a footer', () => {
+    expect(element(by.css('footer')).isPresent()).toBeTruthy();
+  });
 });
