812ae107d30dba64e7dc1bdf9041f05410b74f6b
Janne Mäki <janne.maki@evident.fi>
2016-08-11 16:16:15 +0300

    Test that groups are navigable

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -11,4 +11,9 @@ describe('App', () => {
   it('should have a footer', () => {
     expect(element(by.css('footer')).isPresent()).toBeTruthy();
   });
+
+  it('group should be navigable', () => {
+    element(by.id('browse-panel')).all(by.css('li')).first().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:9001/#/group?urn=https:%2F%2Ftt.eduuni.fi%2Fsites%2Fcsc-iow%23JHS');
+  });
 });
