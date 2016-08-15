a42275270adcec5c0ca11495801771d7861e068e
Janne Mäki <janne.maki@evident.fi>
2016-08-15 17:54:07 +0300

    Test model creation and removal

--- e2e/model.e2e.ts
+++ e2e/model.e2e.ts
@@ -1,14 +1,30 @@
 import { GroupPage } from './pages/group.po';
 import { NavBar } from './components/navbar.po';
+import { applicationUrl } from './utils/constants';
+import { Language } from '../src/utils/language';
+import { ModelPage } from './pages/model.po';
+import { Type } from '../src/services/entities';
+
+function expectCurrentUrlToEqualPath(path: string) {
+  expect(browser.getCurrentUrl().then(decodeURIComponent)).toEqual(decodeURIComponent(applicationUrl + path));
+}
 
 describe('Model', () => {
 
-  const page = new GroupPage();
+  const groupPage = new GroupPage();
   const navbar = new NavBar();
 
-  beforeEach(() => page.navigate(page.JHS_ID));
+  beforeEach(() => groupPage.navigate(groupPage.JHS_ID));
 
   describe('model creation', () => {
+
+    const values = {
+      label: 'E2E Kirjasto',
+      prefix: 'e2e',
+      language: ['fi', 'en'] as Language[],
+      groupId: groupPage.JHS_ID,
+      type: 'library' as Type
+    };
 
     beforeEach(() => navbar.ensureLoggedIn());
 
@@ -17,8 +33,31 @@ describe('Model', () => {
     });
 
     it('can open model creation', () => {
-      const addModelModal = page.addLibrary();
+      const addModelModal = groupPage.addLibrary();
       expect(addModelModal.title.getText()).toBe('Lisää tietokomponenttikirjasto');
+      addModelModal.close();
+      expect(addModelModal.isClosed()).toBe(true);
+    });
+
+    it('creates model', () => {
+      const addModelModal = groupPage.addLibrary();
+
+      addModelModal.setValues(values);
+
+      const modelPage = addModelModal.submit();
+      expect(modelPage.modelView.title.getText()).toBe(values.label);
+      expectCurrentUrlToEqualPath(modelPage.pathToNewModel(values));
+
+      modelPage.modelView.buttons.saveButton.click();
+      expectCurrentUrlToEqualPath(modelPage.pathToExistingModel(modelPage.modelIdForPrefix(values.prefix)));
+    });
+
+    it ('removes model', () => {
+      const modelPage = new ModelPage(values.type);
+      modelPage.navigateToExistingModel(modelPage.modelIdForPrefix(values.prefix));
+      modelPage.modelView.ensureOpen();
+      modelPage.modelView.buttons.removeAndConfirm();
+      expectCurrentUrlToEqualPath(groupPage.path(groupPage.JHS_ID));
     });
   });
 });
