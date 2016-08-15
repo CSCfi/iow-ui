b50456280276efb274ec956e24116b62bce4108a
Janne Mäki <janne.maki@evident.fi>
2016-08-15 17:51:42 +0300

    Confirmation modal page object

---
+++ e2e/modal/confirmationModal.po.ts
@@ -0,0 +1,15 @@
+import { Modal } from './modal.po';
+
+export class ConfirmationModal extends Modal {
+
+  confirmationButton = this.element.element(by.buttonText('Kyllä'));
+  cancelButton = this.element.element(by.buttonText('Peruuta'));
+
+  confirm() {
+    this.confirmationButton.click();
+  }
+
+  cancel() {
+    this.cancelButton.click();
+  }
+}
