c2db6fc0ec965f8f9cb373502ee2cea498034096
Janne Mäki <janne.maki@evident.fi>
2016-08-15 17:52:48 +0300

    Modal close implemented

--- e2e/modal/modal.po.ts
+++ e2e/modal/modal.po.ts
@@ -1,9 +1,20 @@
+import Key = protractor.Key;
+
 export class Modal {
 
-  title = element(by.css('modal-title'));
+  element = element(by.css('.modal-dialog'));
+  title = this.element.$('modal-title');
 
   constructor() {
     // TODO better check for when angular template is rendered fully
     browser.sleep(200);
+  }
+
+  close() {
+    browser.actions().sendKeys(Key.ESCAPE).perform();
+  }
+
+  isClosed() {
+    return this.element.isPresent().then(x => !x);
   }
 }
