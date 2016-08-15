07ba7707395df6a8b131a880958ab2b41eb8e195
Janne Mäki <janne.maki@evident.fi>
2016-08-15 17:52:23 +0300

    Add model modal fillable and submitable

--- e2e/modal/addModelModal.po.ts
+++ e2e/modal/addModelModal.po.ts
@@ -1,4 +1,31 @@
 import { Modal } from './modal.po';
+import { Type } from '../../src/services/entities';
+import { EditableComponent } from '../components/editableComponent.po';
+import { upperCaseFirst } from 'change-case';
+import { ModelPage } from '../pages/model.po';
+import { Language } from '../../src/utils/language';
+import { MultipleEditableComponent } from '../components/multipleEditableComponent.po';
 
 export class AddModelModal extends Modal {
+
+  prefix = EditableComponent.byTitleLocalizationKey('Prefix');
+  label: EditableComponent;
+  language = MultipleEditableComponent.byElementNameAndTitleLocalizationKey('editable-multiple-language-select', 'Model languages');
+  submitButton = element(by.buttonText('Luo uusi'));
+
+  constructor(private type: Type) {
+    super();
+    this.label = EditableComponent.byTitleLocalizationKey(upperCaseFirst(type) + ' label');
+  }
+
+  setValues(values: { prefix: string, label: string, language: Language[] }) {
+    this.prefix.setValue(values.prefix);
+    this.label.setValue(values.label);
+    this.language.setItems(values.language);
+  }
+
+  submit() {
+    this.submitButton.click();
+    return new ModelPage(this.type);
+  }
 }
