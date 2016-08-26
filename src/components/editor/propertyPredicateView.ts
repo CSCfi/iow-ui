97b7c7eda6117c70fb0a83b33de3d8ff7a535138
Janne Mäki <janne.maki@evident.fi>
2016-08-26 11:09:34 +0300

    Allow change of property reusable predicate
    Closes #467

--- src/components/editor/propertyPredicateView.ts
+++ src/components/editor/propertyPredicateView.ts
@@ -4,46 +4,61 @@ import ILocationService = angular.ILocationService;
 import IScope = angular.IScope;
 import ITimeoutService = angular.ITimeoutService;
 import gettextCatalog = angular.gettext.gettextCatalog;
-import { Predicate, Model } from '../../services/entities';
+import { Predicate, Model, Property } from '../../services/entities';
 import { Uri } from '../../services/uri';
 import { module as mod }  from './module';
 import { PredicateService } from '../../services/predicateService';
+import { SearchPredicateModal } from './searchPredicateModal';
+import { createDefinedByExclusion } from '../../utils/exclusion';
+import { ClassFormController } from './classForm';
 
 mod.directive('propertyPredicateView', () => {
   return {
     scope: {
-      predicateOrId: '=predicate',
+      property: '=',
       model: '='
     },
     restrict: 'E',
     template: require('./propertyPredicateView.html'),
     controllerAs: 'ctrl',
     bindToController: true,
-    controller: PropertyPredicateViewController
+    controller: PropertyPredicateViewController,
+    require: ['propertyPredicateView', '^classForm'],
+    link($scope: IScope, element: JQuery, attributes: IAttributes, [thisController, classFormController]: [PropertyPredicateViewController, ClassFormController]) {
+      thisController.isEditing = () => classFormController.isEditing();
+    }
   };
 });
 
 
 class PropertyPredicateViewController {
 
-  predicateOrId: Predicate|Uri;
+  property: Property;
   model: Model;
   predicate: Predicate;
+  isEditing: () => boolean;
 
   /* @ngInject */
-  constructor(private predicateService: PredicateService) {
-    const predicate = this.predicateOrId;
+  constructor($scope: IScope, predicateService: PredicateService, private searchPredicateModal: SearchPredicateModal) {
 
-    if (predicate instanceof Predicate) {
-      this.predicate = predicate;
-    } else if (predicate instanceof Uri) {
-      if (this.model.isNamespaceKnownToBeNotModel(predicate.namespace)) {
-        predicateService.getExternalPredicate(predicate, this.model).then(p => this.predicate = p);
+    $scope.$watch(() => this.property && this.property.predicate, predicate => {
+      if (predicate instanceof Predicate) {
+        this.predicate = predicate;
+      } else if (predicate instanceof Uri) {
+        if (this.model.isNamespaceKnownToBeNotModel(predicate.namespace)) {
+          predicateService.getExternalPredicate(predicate, this.model).then(p => this.predicate = p);
+        } else {
+          predicateService.getPredicate(predicate).then(p => this.predicate = p);
+        }
       } else {
-        predicateService.getPredicate(predicate).then(p => this.predicate = p);
+        throw new Error('Unsupported predicate: ' + predicate);
       }
-    } else {
-      throw new Error('Unsupported predicate: ' + predicate);
-    }
+    });
+  }
+
+  changeReusablePredicate() {
+    this.searchPredicateModal.openWithOnlySelection(this.model, this.predicate.normalizedType, createDefinedByExclusion(this.model)).then(predicate => {
+      this.property.predicate = predicate.id; // Could be full predicate instead of id but this is consistent with api data
+    });
   }
 }
