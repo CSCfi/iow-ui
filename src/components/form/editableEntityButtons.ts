47980b5bbabe721d241815e0af6950be29484657
Janne Mäki <janne.maki@evident.fi>
2016-09-15 11:43:23 +0300

    Adjust buttons width when selection width changes and floating

--- src/components/form/editableEntityButtons.ts
+++ src/components/form/editableEntityButtons.ts
@@ -1,18 +1,25 @@
 import { IScope, IAttributes, IFormController } from 'angular';
 import { EditableEntityController } from './editableEntityController';
 import { module as mod }  from './module';
+import { FloatController } from '../common/float';
 
 mod.directive('editableEntityButtons', () => {
   return {
     restrict: 'E',
     scope: {
       ctrl: '=editableController',
-      context: '='
+      context: '=',
+      width: '='
     },
-    require: '^form',
+    require: ['^form', '?^float'],
     template: require('./editableEntityButtons.html'),
     transclude: true,
-    link($scope: EditableEntityButtonsScope, element: JQuery, attributes: IAttributes, formController: IFormController) {
+    link($scope: EditableEntityButtonsScope, element: JQuery, attributes: IAttributes, [formController, floatController]: [IFormController, FloatController]) {
+      if (floatController) {
+        $scope.$watchGroup([() => $scope.width, () => floatController.floating], ([width, floating]: [number, boolean]) => {
+          floatController.setWidth((floating && width && width - 1) || '');
+        });
+      }
       $scope.form = formController;
     }
   };
@@ -21,4 +28,5 @@ mod.directive('editableEntityButtons', () => {
 interface EditableEntityButtonsScope extends IScope {
   form: IFormController;
   ctrl: EditableEntityController<any>;
+  width?: number;
 }
