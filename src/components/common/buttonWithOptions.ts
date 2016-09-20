a4dafdda4d41b8c2258a7170376c9ac531b7fd5b
Janne Mäki <janne.maki@evident.fi>
2016-09-20 12:27:31 +0300

    Button with addition options implemented

---
+++ src/components/common/buttonWithOptions.ts
@@ -0,0 +1,47 @@
+import { module as mod } from './module';
+
+mod.directive('buttonWithOptions', () => {
+  return {
+    restrict: 'E',
+    scope: {
+      options: '=',
+      disabled: '='
+    },
+    bindToController: true,
+    controllerAs: 'ctrl',
+    controller: ButtonWithOptionsController,
+    template: `                
+        <div ng-if="ctrl.options.length > 1" class="btn-group pull-right" uib-dropdown>
+          <button type="button" 
+                  class="btn btn-default additional"
+                  ng-disabled="ctrl.disabled"
+                  uib-dropdown-toggle><span class="caret"></span></button>
+          <ul class="dropdown-menu" role="menu">
+            <li role="menuitem" ng-repeat="option in ctrl.options"><a ng-click="option.apply()">{{option.name | translate}}</a></li>
+          </ul>
+        </div>
+        
+        <button type="button"
+                class="btn btn-default pull-right"
+                ng-disabled="ctrl.disabled"
+                ng-click="ctrl.options[0].apply()" uib-tooltip="{{ctrl.options[0].name | translate}}"><i class="fa fa-repeat"></i></button>
+    `
+  };
+});
+
+export interface Option {
+  name: string;
+  apply: () => void;
+}
+
+class ButtonWithOptionsController {
+
+  options: Option[];
+  disabled: boolean;
+
+  constructor() {
+    if (!this.options || this.options.length === 0) {
+      throw new Error('Empty options');
+    }
+  }
+}
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -14,6 +14,7 @@ import './accordion';
 import './accordionChevron';
 import './ajaxLoadingIndicator';
 import './ajaxLoadingIndicatorSmall';
+import './buttonWithOptions';
 import './clipboard';
 import './export';
 import './float';
