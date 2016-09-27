ecd9ce7fdc35ae859b6ab26a743a0f11f9fa8bcb
Janne Mäki <janne.maki@evident.fi>
2016-09-27 22:00:53 +0300

    Simplify

--- src/components/common/highlight.ts
+++ src/components/common/highlight.ts
@@ -26,64 +26,17 @@ function applyHighlight(text: string, search: string): string {
   if (!search || search.length === 0) {
     return text;
   } else {
-    const reducer = (result: string, token: Token) =>
-      result + (token.isMatch ? `<span class="highlight">${token.text}</span>` : token.text);
 
     if (!cachedRegex || cachedRegex.search !== search) {
-      cachedRegex = { search, value: createRegex(search, 'i') };
+      cachedRegex = { search, value: createRegex(search, 'gi') };
     }
 
-    return resolveTokens(text, cachedRegex.value).reduce(reducer, '');
+    return text.replace(cachedRegex.value, '<span class="highlight">$1</span>');
   }
-}
-
-interface Token {
-  text: string;
-  isMatch: boolean;
-}
-
-function resolveTokens(text: string, search: RegExp) {
-
-  const tokens: Token[] = [];
-
-  const addToken = (t: string, match: boolean) => {
-    tokens.push({
-      isMatch: match,
-      text: t
-    });
-  };
-
-  let t = text.slice();
-
-  while (true) {
-
-    const matches = t.match(search);
-
-    if (matches && matches.length > 0) {
-      const startIndex = matches.index;
-      const endIndex = startIndex + matches[0].length;
-
-      if (startIndex > 0) {
-        addToken(t.slice(0, startIndex), false);
-      }
-
-      addToken(t.slice(startIndex, endIndex), true);
-
-      t = t.substring(endIndex);
-    } else {
-      break;
-    }
-  }
-
-  if (t.length > 0) {
-    addToken(t, false);
-  }
-
-  return tokens;
 }
 
 function createRegex(term: string, flags: string) {
-  return new RegExp(sanitizeRegex(term), flags);
+  return new RegExp('(' + sanitizeRegex(term) + ')', flags);
 }
 
 function sanitizeRegex(term: string) {
