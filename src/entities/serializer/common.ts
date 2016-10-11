62862573517a8a26dd7ff9e04663196d1393ce25
Janne Mäki <janne.maki@evident.fi>
2016-10-11 14:13:29 +0300

    Minor

--- src/entities/serializer/common.ts
+++ src/entities/serializer/common.ts
@@ -5,5 +5,7 @@ import { entityAsId, normalized } from './entitySerializer';
 import { WithIdAndType } from '../../components/contracts';
 import { GraphNode } from '../graphNode';
 
-export const normalizingDefinedBySerializer = normalized<DefinedBy, GraphNode & WithIdAndType>(entityAsId(() => DefinedBy),
-  (data, instance) => requireDefined(normalizeAsSingle(data, instance.id)));
+export const normalizingDefinedBySerializer = normalized<DefinedBy, GraphNode & WithIdAndType>(
+  entityAsId(() => DefinedBy),
+  (data, instance) => requireDefined(normalizeAsSingle(data, instance.id))
+);
