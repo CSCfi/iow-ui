dfc56b23877971dca58c76507f9e1ecf83ff592f
Janne Mäki <janne.maki@evident.fi>
2016-10-17 00:33:42 +0300

    Remove unnecessary comma

--- src/components/common/interactiveHelp.ts
+++ src/components/common/interactiveHelp.ts
@@ -309,7 +309,7 @@ class InteractiveHelpController {
             top: { left: 0, top: 0, right: 0, bottom: 0 },
             right: { left: 0, top: 0, width: 0, height: 0 },
             bottom: { left: 0, top: 0, width: 0, height: 0 },
-            left: { left: 0, top: 0, width: 0, height: 0 },
+            left: { left: 0, top: 0, width: 0, height: 0 }
           };
         } else {
           this.backdrop = null;
