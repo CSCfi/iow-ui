7a9160a6c1b0b7d0d7299f2f9f672337a3ece04f
Janne Mäki <janne.maki@evident.fi>
2016-10-26 14:25:24 +0300

    Optional namespace require on model init

--- src/help/modelPageHelp.ts
+++ src/help/modelPageHelp.ts
@@ -1,4 +1,4 @@
-import { ILocationService, ui } from 'angular';
+import { IQService, ILocationService, ui } from 'angular';
 import IModalStackService = ui.bootstrap.IModalStackService;
 import {
   StoryLine, InteractiveHelp, createNotification
@@ -11,7 +11,7 @@ import { filterForModel, selectResult } from './pages/model/modal/addEditNamepac
 import { Model } from '../entities/model';
 import { InteractiveHelpService } from './services/interactiveHelpService';
 import { KnownModelType } from '../entities/type';
-import { assertNever } from '../utils/object';
+import { assertNever, requireDefined } from '../utils/object';
 
 export const addNamespaceItems = [
   requireNamespace,
@@ -41,7 +41,7 @@ export function addNamespace(type: KnownModelType) {
 export class ModelPageHelpService {
 
   /* @ngInject */
-  constructor(private $uibModalStack: IModalStackService, private $location: ILocationService) {
+  constructor(private $q: IQService, private $uibModalStack: IModalStackService, private $location: ILocationService) {
   }
 
   private returnToModelPage(model: Model) {
@@ -49,17 +49,40 @@ export class ModelPageHelpService {
     this.$location.url(model.iowUrl());
   }
 
-  private initialize(model: Model) {
+  private initialize(model: Model, requirePrefix?: string) {
 
     return (service: InteractiveHelpService) => {
 
+      const persistModel = (newModel: Model) => {
+        service.helpModelService.createModel(newModel);
+        return newModel;
+      };
+
+      const addNamespace = (newModel: Model) => {
+        if (requirePrefix) {
+          return service.helpModelService.getAllImportableNamespaces()
+            .then(namespaces => {
+
+              const jhs = requireDefined(namespaces.find(ns => ns.prefix === requirePrefix));
+              newModel.addNamespace(jhs);
+
+              return newModel;
+            });
+        } else {
+          return this.$q.when(newModel);
+        }
+      };
+
+      const navigate = (newModel: Model) => {
+        this.$location.url(newModel.iowUrl());
+        return true;
+      };
+
       const createModelAndNavigate = (prefix: string, label: string) => {
         return service.helpModelService.newModel(prefix, label, model.groupId, model.language, model.normalizedType)
-          .then(newModel => {
-            service.helpModelService.createModel(newModel);
-            this.$location.url(newModel.iowUrl());
-            return true;
-          });
+          .then(addNamespace)
+          .then(persistModel)
+          .then(navigate);
       };
 
       return service.reset().then(() => {
@@ -75,10 +98,10 @@ export class ModelPageHelpService {
     };
   }
 
-  private createHelp(model: Model, storyLine: StoryLine) {
+  private createHelp(model: Model, storyLine: StoryLine, requirePrefix?: string) {
     return {
       storyLine,
-      onInit: this.initialize(model),
+      onInit: this.initialize(model, requirePrefix),
       onComplete: () => this.returnToModelPage(model),
       onCancel: () => this.returnToModelPage(model)
     };
