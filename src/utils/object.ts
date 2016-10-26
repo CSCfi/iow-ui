cee032a47d02b15599d596bd36fa4fd6a59fd1f2
Janne Mäki <janne.maki@evident.fi>
2016-10-26 15:36:30 +0300

    Use optional type

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -6,7 +6,7 @@ export function referenceEquality<T>(lhs: T, rhs: T) {
   return lhs === rhs;
 }
 
-export function areEqual<T>(lhs: T|null|undefined, rhs: T|null|undefined, equals: EqualityChecker<T> = referenceEquality): boolean {
+export function areEqual<T>(lhs: Optional<T>, rhs: Optional<T>, equals: EqualityChecker<T> = referenceEquality): boolean {
   if ((isDefined(lhs) && !isDefined(rhs)) || (!isDefined(lhs) && isDefined(rhs))) {
     return false;
   } else if (!isDefined(lhs) && !isDefined(rhs)) {
@@ -16,11 +16,11 @@ export function areEqual<T>(lhs: T|null|undefined, rhs: T|null|undefined, equals
   }
 }
 
-export function isDefined<T>(obj: T|undefined|null): obj is T {
+export function isDefined<T>(obj: Optional<T>): obj is T {
   return obj !== null && obj !== undefined;
 }
 
-export function mapOptional<T, R>(obj: T|null|undefined, fn: (obj: T) => R): R|null {
+export function mapOptional<T, R>(obj: Optional<T>, fn: (obj: T) => R): R|null {
   if (isDefined(obj)) {
     return fn(obj);
   } else {
@@ -74,7 +74,7 @@ export function assertNever(_x: never, msg?: string): never {
   throw new Error(msg);
 }
 
-export function requireDefined<T>(obj: T|undefined|null, msg?: string): T {
+export function requireDefined<T>(obj: Optional<T>, msg?: string): T {
   if (!isDefined(obj)) {
     throw new Error('Object must not be null or undefined: ' + msg);
   }
