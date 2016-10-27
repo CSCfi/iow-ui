5527b6b5700ddb986ac68cda74ec0e59fcb3bd47
Janne Mäki <janne.maki@evident.fi>
2016-10-27 20:02:31 +0300

    Helpers for selecting child indices

--- src/help/selectors.ts
+++ src/help/selectors.ts
@@ -1,4 +1,4 @@
-export const modal = () => angular.element('.modal-dialog').eq(0);
+export const modal = () => angular.element('.modal-dialog');
 export const modalBody = child(modal, '.modal-body');
 export const modelView = () => angular.element('model-view');
 export const classView = () => angular.element('class-view');
@@ -7,7 +7,19 @@ export function child(parent: () => JQuery, selector: string) {
   return () => parent().find(selector);
 }
 
-export const input =                       (parent: () => JQuery) => child(parent, 'input');
+export function nth(element: () => JQuery, index: number) {
+  return () => element().eq(index);
+}
+
+export function first(element: () => JQuery) {
+  return nth(element, 0);
+}
+
+export function second(element: () => JQuery) {
+  return nth(element, 1);
+}
+
+export const input =                       (parent: () => JQuery) => child(parent, 'input,select,textarea');
 export const multiInput =                  (parent: () => JQuery) => child(parent, '.multi-input');
 export const editableFocus =               (parent: () => JQuery) => child(parent, '.editable-wrap');
 export const editableByTitle =             (parent: () => JQuery, title: string) => child(parent, `editable[data-title="${title}"]`);
