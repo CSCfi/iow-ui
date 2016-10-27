c4f9ba95a15ff3aa5905cbabf5c08eda089f02eb
Janne Mäki <janne.maki@evident.fi>
2016-10-27 20:03:38 +0300

    Forgot to commit help service di registration

--- src/help/index.ts
+++ src/help/index.ts
@@ -3,7 +3,9 @@ export default mod.name;
 
 import { InteractiveHelpService } from './services/interactiveHelpService';
 import { InteractiveHelpModelService } from './services/helpModelService';
+import { InteractiveHelpClassService } from './services/helpClassService';
 import { InteractiveHelpUserService } from './services/helpUserService';
+import { InteractiveHelpVocabularyService } from './services/helpVocabularyService';
 import { InteractiveHelpVisualizationService } from './services/helpVisualizationService';
 
 import { FrontPageHelpService } from './frontPageHelp';
@@ -11,13 +13,13 @@ import { GroupPageHelpService } from './groupPageHelp';
 import { ModelPageHelpService } from './modelPageHelp';
 
 import { InteractiveHelpDisplay } from './components/interactiveHelpDisplay';
-import { InteractiveHelpClassService } from './services/helpClassService';
 
 mod.service('interactiveHelpService', InteractiveHelpService);
 mod.service('helpModelService', InteractiveHelpModelService);
 mod.service('helpClassService', InteractiveHelpClassService);
-mod.service('helpVisualizationService', InteractiveHelpVisualizationService);
 mod.service('helpUserService', InteractiveHelpUserService);
+mod.service('helpVocabularyService', InteractiveHelpVocabularyService);
+mod.service('helpVisualizationService', InteractiveHelpVisualizationService);
 
 mod.service('frontPageHelpService', FrontPageHelpService);
 mod.service('groupPageHelpService', GroupPageHelpService);
