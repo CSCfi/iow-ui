2f6897ec8ba52d2be965a194c747fb6bcc2a4162
Janne Mäki <janne.maki@evident.fi>
2016-11-07 19:02:20 +0200

    Fix hot loading

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -3,21 +3,32 @@ import * as WebpackDevServer from 'webpack-dev-server';
 import { serveConfig } from '../webpack.config';
 import { applyProgressBar } from './webpackProgressBar';
 
-const compiler = applyProgressBar(webpack(serveConfig));
+const hostname = 'localhost';
+const port = 9001;
+const apiPort = 8084;
+
+function applyHotLoading(config: webpack.Configuration) {
+  return Object.assign({}, config, {
+    entry: Object.assign({}, config.entry, {
+      dev: `webpack-dev-server/client?http://${hostname}:${port}/`,
+      hot: 'webpack/hot/dev-server'
+    })
+  });
+}
+
+const compiler = applyProgressBar(webpack(applyHotLoading(serveConfig)));
+
 const server = new WebpackDevServer(compiler, {
-  stats: { colors: true },
-  host: '0.0.0.0',
-  port: 9001,
+  stats: true,
   contentBase: './src',
   hot: true,
-  inline: true,
   historyApiFallback: '/',
   proxy: {
     '/api/*': {
-      target: 'http://localhost:8084/',
+      target: `http://${hostname}:${apiPort}/`,
       secure: false
     }
   }
 });
 
-server.listen(9001, '0.0.0.0');
+server.listen(port, '0.0.0.0');
